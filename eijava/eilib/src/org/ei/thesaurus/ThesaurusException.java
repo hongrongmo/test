74b68b7d596865db048e152f46841a454da9b5f0
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2007-05-09 19:37:02 +0000

    Initial revision.
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@3399 aa8a525f-4958-0410-8862-b993faadc08d

---
+++ eijava/eilib/src/org/ei/thesaurus/ThesaurusException.java
@@ -0,0 +1,13 @@
+package org.ei.thesaurus;
+
+import org.ei.exception.BasicException;
+
+public class ThesaurusException extends BasicException
+{
+
+	public ThesaurusException(Exception e)
+	{
+		super(e);
+	}
+
+}
