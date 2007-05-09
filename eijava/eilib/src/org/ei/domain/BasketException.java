ec289d2942f498dbfbea387ecee967a0731c9c57
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2007-05-09 19:35:30 +0000

    Initial revision.
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@3353 aa8a525f-4958-0410-8862-b993faadc08d

---
+++ eijava/eilib/src/org/ei/domain/BasketException.java
@@ -0,0 +1,14 @@
+package org.ei.domain;
+
+import org.ei.exception.BasicException;
+
+public class BasketException 
+	extends BasicException
+{
+
+	public BasketException(Exception e)
+	{
+		super(e);
+	}
+
+}
