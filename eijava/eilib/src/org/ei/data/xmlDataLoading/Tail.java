c622792c9378cdc4898dad2bbe5cb428bea5a634
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2007-05-09 19:35:24 +0000

    Initial revision.
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@3350 aa8a525f-4958-0410-8862-b993faadc08d

---
+++ eijava/eilib/src/org/ei/data/xmlDataLoading/Tail.java
@@ -0,0 +1,19 @@
+package org.ei.data.xmlDataLoading;
+
+public class Tail extends BaseElement
+{
+	Bibliography bibliography;
+
+	public void setBibliography(Bibliography bibliography)
+	{
+		this.bibliography = bibliography;
+	}
+
+	public Bibliography getBibliography()
+	{
+		return bibliography;
+	}
+
+
+
+}
