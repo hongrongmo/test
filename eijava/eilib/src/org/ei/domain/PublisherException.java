b1213aea8206a33bfaf3f6e91b4ab8a710657bd8
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2007-05-09 19:35:50 +0000

    Initial revision.
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@3363 aa8a525f-4958-0410-8862-b993faadc08d

---
+++ eijava/eilib/src/org/ei/domain/PublisherException.java
@@ -0,0 +1,12 @@
+package org.ei.domain;
+
+import org.ei.exception.BasicException;
+
+public class PublisherException
+	extends BasicException
+{
+	public PublisherException(Exception e)
+	{
+		super(e);
+	}
+}
---
+++ eijava/eilib/src/org/ei/domain/SearchException.java
@@ -0,0 +1,12 @@
+package org.ei.domain;
+
+import org.ei.exception.BasicException;
+
+public class SearchException 
+	extends BasicException
+{
+	public SearchException(Exception e)
+	{
+		super(e);
+	}
+}
