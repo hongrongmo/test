a598d58975b2a3e5eccae8b385dcc36dc63ddd52
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2007-05-09 19:35:48 +0000

    Initial revision.
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@3362 aa8a525f-4958-0410-8862-b993faadc08d

---
+++ eijava/eilib/src/org/ei/domain/PageCacheException.java
@@ -0,0 +1,17 @@
+
+package org.ei.domain;
+
+import org.ei.exception.BasicException;
+
+
+public class PageCacheException 
+	extends BasicException
+{
+
+
+	public PageCacheException(Exception e)
+	{
+		super(e);
+	}
+	
+}
