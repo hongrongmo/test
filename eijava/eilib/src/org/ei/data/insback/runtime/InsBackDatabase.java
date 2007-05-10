06e26cceee2911eed36c0fe871573fe0b708d065
hmo <hmo@aa8a525f-4958-0410-8862-b993faadc08d>
2007-05-10 14:20:08 +0000

    Initial revision.
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@3629 aa8a525f-4958-0410-8862-b993faadc08d

---
+++ eijava/eilib/src/org/ei/data/insback/runtime/InsBackDatabase.java
@@ -0,0 +1,118 @@
+package org.ei.ev.driver.insback;
+
+import org.ei.domain.*;
+import java.util.Map;
+import java.util.Hashtable;
+import org.ei.ev.driver.cpx.CPXLinkingStrategy;
+import org.ei.fulldoc.LinkingStrategy;
+public class InsBackDatabase extends Database
+{
+    private DataDictionary dataDictionary = new org.ei.ev.driver.inspec.InspecDataDictionary();
+    public int getStartYear(boolean hasBackFile) { return 1896; }
+
+    private LinkingStrategy linkingStrategy = new IBFLinkingStrategy();
+    protected String getBaseTableHook()
+    {
+        return "ibf_master";
+    }
+
+
+    public DocumentBuilder newBuilderInstance()
+    {
+        return new InsBackDocBuilder(this);
+    }
+
+    public SearchControl newSearchControlInstance()
+    {
+        return new FastSearchControl();
+    }
+
+    /*
+    * Because this a backfile overide getLastFourUpdates so that
+    * never returns a hit from FAST.
+    */
+
+    public String getUpdates(int num)
+        throws SearchException
+    {
+        return "13000-13001";
+    }
+
+    public DataDictionary getDataDictionary()
+    {
+        return dataDictionary;
+    }
+
+
+    public String getID()
+    {
+        return "ibf";
+    }
+
+    public String getName()
+    {
+        return "Inspec";
+    }
+
+    public String getIndexName()
+    {
+        return "ibf";
+    }
+    public boolean isBackfile()
+    {
+        return true;
+    }
+
+    public String getShortName()
+    {
+        return "Inspec";
+    }
+
+ 	public boolean hasField(SearchField searchField)
+  	{
+	 	return false;
+  	}
+
+	public LinkingStrategy getLinkingStrategy()
+	{
+	    return linkingStrategy;
+  	}
+
+    public int getMask()
+    {
+        return 4096;
+    }
+    public int getSortValue(){ return 2; }
+
+  public Map getTreatments() {
+
+    Map mp = new Hashtable();
+
+    mp.put("A", "Applications (APP)");
+        mp.put("B", "Bibliography (BIB)");
+        mp.put("E", "Economic (ECO)");
+        mp.put("G", "General review (GEN)");
+        mp.put("N", "New development (NEW)");
+        mp.put("P", "Practical (PRA)");
+        mp.put("R", "Product review (PRO)");
+        mp.put("T", "Theoretical (THR)");
+        mp.put("X", "Experimental (EXP)");
+
+    return mp;
+  }
+
+	public boolean linkLocalHoldings(String linklabel)
+	{
+		if(linklabel.indexOf("NTIS") > -1)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+
+
+
+}
