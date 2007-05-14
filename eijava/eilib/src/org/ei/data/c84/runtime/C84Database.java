a7342a2583c441f854b79d04655ba2718cdf97c1
hmo <hmo@aa8a525f-4958-0410-8862-b993faadc08d>
2007-05-14 19:05:10 +0000

    Initial revision.
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@3766 aa8a525f-4958-0410-8862-b993faadc08d

---
+++ eijava/eilib/src/org/ei/data/c84/runtime/C84Database.java
@@ -0,0 +1,99 @@
+package org.ei.data.c84.runtime;
+
+import org.ei.domain.*;
+import java.util.Map;
+import java.util.Hashtable;
+import org.ei.fulldoc.LinkingStrategy;
+
+public class C84Database
+	extends Database
+{
+
+    public int getStartYear(boolean hasBackFile) { return 1884; }
+    private LinkingStrategy linkingStrategy = new C84LinkingStrategy();
+    protected String getBaseTableHook()
+    {
+        return "c84_master";
+    }
+
+
+    /*
+    * Because this a backfile overide getLastFourUpdates so that
+    * never returns a hit from FAST.
+    */
+
+    public String getUpdates(int num)
+        throws SearchException
+    {
+        return "13000-13001";
+    }
+
+    public DocumentBuilder newBuilderInstance()
+    {
+        return new C84DocBuilder(this);
+    }
+
+    public SearchControl newSearchControlInstance()
+    {
+        return new FastSearchControl();
+    }
+
+    public String getID()
+    {
+        return "c84";
+    }
+
+    public String getName()
+    {
+        return "Compendex";
+    }
+
+    public boolean isBackfile()
+    {
+        return true;
+    }
+
+    public String getIndexName()
+    {
+        return "c84";
+    }
+
+    public String getShortName()
+    {
+        return "Compendex";
+    }
+
+    public int getMask()
+    {
+        return 32;
+    }
+
+    public LinkingStrategy getLinkingStrategy()
+    {
+        return linkingStrategy;
+    }
+
+ 	public boolean hasField(SearchField searchField)
+  	{
+	 	return false;
+  	}
+    public int getSortValue(){ return 1; }
+
+    public Map getTreatments()
+    {
+    	Map mp = new Hashtable();
+		return mp;
+  	}
+
+	public boolean linkLocalHoldings(String linklabel)
+	{
+		if(linklabel.indexOf("NTIS") > -1)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+}
