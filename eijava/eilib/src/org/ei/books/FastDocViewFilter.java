2e0b8fc2e93301cc95554ff4d5135d7bc2a122f0
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2007-06-13 18:48:36 +0000

    fixed path - removed harcoded /pdfs
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@3839 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/books/FastDocViewFilter.java
+++ eijava/eilib/src/org/ei/books/FastDocViewFilter.java
@@ -20,12 +20,12 @@ import org.apache.commons.logging.LogFactory;
  * This class will inspect the referring URLS and look for MD5 hash
  * The hash will be re-created from the data and the shared secret
  * and compared to the one provided on the referrer.
- *  
+ *
  *
  * Add this to the WEB-INF/classes/log4j.properties
  * to isolate the logging for this class from the rest of the docview servlet
  * classes.
- * 
+ *
  * log4j.appender.F=org.apache.log4j.RollingFileAppender
  * log4j.appender.F.File=${catalina.home}/logs/filter.log
  * log4j.appender.F.MaxFileSize=10MB
@@ -33,14 +33,14 @@ import org.apache.commons.logging.LogFactory;
  * log4j.appender.F.layout=org.apache.log4j.PatternLayout
  * log4j.appender.F.layout.ConversionPattern=%5p [%t] %d{yyyyMMddHHmmss} (%F:%L) - %m%n
  * log4j.logger.org.ei=INFO, F
- * 
+ *
  * Add this to the docview/WEB-INF/web.xml
- * 
+ *
  *   <filter>
  *   <filter-name>FastDocViewFilter</filter-name>
  *   <filter-class>org.ei.books.FastDocViewFilter</filter-class>
  *   </filter>
- *   
+ *
  *   <filter-mapping>
  *   <filter-name>FastDocViewFilter</filter-name>
  *   <url-pattern>/*</url-pattern>
@@ -75,54 +75,54 @@ public final class FastDocViewFilter implements Filter {
 
         HttpServletRequest hreq = (HttpServletRequest) srequest;
 //        String currentURL = hreq.getRequestURL().toString();
-//    	log.info(" currentURL: " + currentURL);        	  
+//    	log.info(" currentURL: " + currentURL);
 
     	String currentURI = hreq.getRequestURI();
 
-        
+
 //        Enumeration paramenum = hreq.getParameterNames();
 //        while(paramenum.hasMoreElements()) {
 //        	String paramname = (String) paramenum.nextElement();
 //        	log.info(paramname + " <= " + hreq.getParameter(paramname));
 //		}
-//        
+//
         if((currentURI != null) && (currentURI.endsWith("pdf")))
         {
         	boolean passed = false;
-        	
+
         	log.info(" currentURI: " + currentURI);
-        	  
+
         	String md5param = hreq.getParameter("TICKET");
             log.info(" ticket = " + md5param);
-
-            String isbn = currentURI.substring(currentURI.indexOf("pdfs/")+5,currentURI.lastIndexOf("/"));
+            String uriprefix = "docview/";
+            String isbn = currentURI.substring(currentURI.indexOf(uriprefix)+uriprefix.length(),currentURI.lastIndexOf("/"));
 //            log.info(" isbn = " + isbn);
 
         	if(md5param != null) {
-	
-                // currentURI format /docview/pdfs/0125816502/pg_0806.pdf
-                String[] tokens = md5param.split(":");
-                if((tokens != null) && tokens.length == 3)
-                {
-            		StringBuffer ticketVal = new StringBuffer(); 
-                	String reqticket = tokens[0];
-                	String custid = tokens[1];
-	                String currentTime = tokens[2];
-	                String newticket = "";
-	                
-	        		ticketVal.append(isbn).append(custid).append(currentTime);
-	        		ticketVal.append(FastDocViewFilter.SHRDKEY);
-	
-	        		MessageDigest md5;
-					try {
-						md5 = MessageDigest.getInstance("MD5");
-						byte[] di = md5.digest(ticketVal.toString().getBytes());
-						newticket = new String(asHex(di));
-						log.info(" HASHED: " + newticket);
-					} catch (NoSuchAlgorithmException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+
+            // currentURI format /docview/0125816502/pg_0806.pdf
+            String[] tokens = md5param.split(":");
+            if((tokens != null) && tokens.length == 3)
+            {
+            		StringBuffer ticketVal = new StringBuffer();
+              	String reqticket = tokens[0];
+              	String custid = tokens[1];
+                String currentTime = tokens[2];
+                String newticket = "";
+
+  	        		ticketVal.append(isbn).append(custid).append(currentTime);
+  	        		ticketVal.append(FastDocViewFilter.SHRDKEY);
+
+  	        		MessageDigest md5;
+      					try {
+      						md5 = MessageDigest.getInstance("MD5");
+      						byte[] di = md5.digest(ticketVal.toString().getBytes());
+      						newticket = new String(asHex(di));
+      						log.info(" HASHED: " + newticket);
+      					} catch (NoSuchAlgorithmException e) {
+      						// TODO Auto-generated catch block
+      						e.printStackTrace();
+      					}
 	        		 passed = reqticket.equals(newticket);
 	        		 if(passed)
 	        		 {
@@ -131,16 +131,21 @@ public final class FastDocViewFilter implements Filter {
 	        				 // Test if time diff is greater than 30 minutes
 		        			 if((System.currentTimeMillis() - (Long.parseLong(currentTime))) > 1800000)
 		        			 {
+		        			    log.info("Time expired on ticket.");
 		        				 passed = false;
 		        			 }
 	        			 }
 	        			 catch (NumberFormatException e) {
 	        				 passed = false;
-	        			 }							
-					
+	        			 }
+
 	        		 }
-                }
-			}
+	        		 else
+	        		 {
+                  log.info("Ticket mismatch.");
+               }
+            }
+			    }
 	        else
 	        {
 	        	passed = false;
@@ -167,5 +172,5 @@ public final class FastDocViewFilter implements Filter {
 		}
 		return buf.toString();
 	}
-	
+
 }
