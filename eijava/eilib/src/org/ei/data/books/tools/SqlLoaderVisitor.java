42289136f8c6243f781f8cb3ef78776d9d9604e2
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2007-07-10 17:46:00 +0000

    added 'Zero' record for each book
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@3923 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/data/books/tools/SqlLoaderVisitor.java
+++ eijava/eilib/src/org/ei/data/books/tools/SqlLoaderVisitor.java
@@ -14,11 +14,88 @@ public class SqlLoaderVisitor extends BookVisitor {
     }
     
     public void visit(PDF_FileInfo pdffile) {
+        // Write Page 0 - "Book Record"
+        int pageZero = 0;
+        
+        try {
+            // DOCID
+            wrtr.write("pag_" + pdffile.getIsbn13().toLowerCase()+"_"+String.valueOf(pageZero));
+            wrtr.write(FIELD_DELIMITER);
+            // BN 
+            wrtr.write(pdffile.getIsbn().toLowerCase());
+            wrtr.write(FIELD_DELIMITER);
+            // BN13
+            wrtr.write(pdffile.getIsbn13().toLowerCase());
+            wrtr.write(FIELD_DELIMITER);
+
+            // PII - Only if this page is part of a Chapter/Downloadable "CHUNK"
+            wrtr.write(FIELD_DELIMITER);
+            // PAGE_NUM
+            wrtr.write(String.valueOf(pageZero));
+            wrtr.write(FIELD_DELIMITER);
+            // PAGE_LABEL
+            wrtr.write(String.valueOf(pageZero));
+            wrtr.write(FIELD_DELIMITER);
+
+            // SECTION_TITLE
+            wrtr.write("\"Book\"");
+            wrtr.write(FIELD_DELIMITER);
+            // SECTION_START
+            wrtr.write(String.valueOf(pageZero));
+            wrtr.write(FIELD_DELIMITER);
+
+            // CHAPTER_TITLE - Only if this page is part of a Chapter/Downloadable "CHUNK"
+            wrtr.write("\"Book\"");
+            wrtr.write(FIELD_DELIMITER);
+            
+            // CHAPTER_START - Only if this page is part of a Chapter/Downloadable "CHUNK"
+            wrtr.write(String.valueOf(pageZero));
+            wrtr.write(FIELD_DELIMITER);
+
+            // PAGE_BYTES
+            wrtr.write(Long.toString(0));
+            wrtr.write(FIELD_DELIMITER);
+
+            // PAGE_TEXT
+            wrtr.write("");
+            wrtr.write(FIELD_DELIMITER);
+            
+            // PAGE_TOTAL
+            wrtr.write(String.valueOf(pdffile.getPageCount()));
+            wrtr.write(FIELD_DELIMITER);
+            
+            // PAGE_KEYWORDS
+            wrtr.write(FIELD_DELIMITER);
+            
+            wrtr.write(FIELD_DELIMITER); // PP
+            wrtr.write(FIELD_DELIMITER); // YR
+            wrtr.write(FIELD_DELIMITER); // TI
+            wrtr.write(FIELD_DELIMITER); // AUS
+            wrtr.write(FIELD_DELIMITER); // CVS
+            wrtr.write(FIELD_DELIMITER); // AF
+            wrtr.write(FIELD_DELIMITER); // PN
+            wrtr.write(FIELD_DELIMITER); // NT
+            wrtr.write(FIELD_DELIMITER); // ST
+            wrtr.write(FIELD_DELIMITER); // SP
+            wrtr.write(FIELD_DELIMITER); // ISS
+            wrtr.write(FIELD_DELIMITER); // VO
+            wrtr.write(FIELD_DELIMITER); // AB
+            wrtr.write(FIELD_DELIMITER); // SUB
+
+            //PDFPP
+            wrtr.write(String.valueOf(pdffile.getPageCount()));
+          
+            
+            wrtr.write(System.getProperty("line.separator"));
+        } catch (IOException e) {
+            // TODO: handle exception
+        }
+        
+        // loop through all PDF pages
         for (int curpage = 1; curpage <= pdffile.getPageCount(); curpage++) {
 
             PDF_Page referexpage = pdffile.getPage(curpage);
         
-// skip this for now for speeding up
             long page_size = 0; //referexpage.getTextSizeBytes();
             
             try {
