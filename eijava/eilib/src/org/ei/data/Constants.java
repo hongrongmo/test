cfe131eddd90590e4a1b3b457db2a9d82b2a447c
tsolovye <tsolovye@aa8a525f-4958-0410-8862-b993faadc08d>
2007-08-29 15:54:00 +0000

    Initial revision.
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@3981 aa8a525f-4958-0410-8862-b993faadc08d

---
+++ eijava/eilib/src/org/ei/data/Constants.java
@@ -0,0 +1,45 @@
+package org.ei.data;
+
+
+public class Constants
+{
+    private String constant;
+    public static final Constants CVS = new Constants("CVS");
+    public static final Constants DT = new Constants("DT");
+    public static final Constants DT_UA = new Constants("ua");
+    public static final Constants DT_UG = new Constants("ug");
+    public static final Constants CONF_PAPER = new Constants("_CP");
+    public static final Constants CONF_PAPER_EXPAND = new Constants("_COP");
+    public static final Constants IPC = new Constants("IP");
+    public static final Constants MULTIFIELD_DELIM = new Constants(";");
+    public static final Constants PN = new Constants("PN");
+    public static final Constants PN_FIX = new Constants("0");
+    public static final Constants US = new Constants("US");
+    
+
+    private Constants(String constant)
+    {
+        this.constant = constant;
+    }
+
+    public String toString()
+    {
+        return this.constant;
+    }
+
+    public String getValue()
+    {
+        return this.constant;
+    }
+
+    public boolean equals(Object o)
+    {
+    	Constants f = (Constants)o;
+
+        if(f.constant.equals(this.constant))
+        {
+            return true;
+        }
+        return false;
+    }
+}
