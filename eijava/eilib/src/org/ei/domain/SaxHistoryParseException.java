88992a2013dcb8d09004895d41bfbb5d4d547307
tsolovye <tsolovye@aa8a525f-4958-0410-8862-b993faadc08d>
2007-09-26 18:50:34 +0000

    Initial revision.
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@4067 aa8a525f-4958-0410-8862-b993faadc08d

---
+++ eijava/eilib/src/org/ei/domain/SaxHistoryParseException.java
@@ -0,0 +1,13 @@
+package org.ei.domain;
+
+import org.ei.exception.BasicException;
+
+public class SaxHistoryParseException
+	extends BasicException
+{
+
+	public SaxHistoryParseException(Exception e)
+	{
+		super(e);
+	}
+}
