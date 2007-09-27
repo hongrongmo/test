58b86b2ccc3b1717d15cb19c31443d688e099b16
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2007-09-27 16:13:54 +0000

    use referexcollections so that collection names are not hardcoded
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@4080 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/domain/personalization/GlobalLinks.java
+++ eijava/eilib/src/org/ei/domain/personalization/GlobalLinks.java
@@ -1,7 +1,4 @@
 package org.ei.domain.personalization;
-
-
-
 /*
     UI Helper class
 
@@ -12,78 +9,80 @@ package org.ei.domain.personalization;
 
     Optional Items are passed in in a list (which may be empty!)
 */
+import org.ei.books.collections.ReferexCollection;
+
 public class GlobalLinks {
 
+  public static String toXML(String[] customizedFeatures)
+  {
 
-    public static String toXML(String[] customizedFeatures) {
+    StringBuffer strBufGlobalLinksXML = new StringBuffer();
 
-        StringBuffer strBufGlobalLinksXML = new StringBuffer();
+    boolean hasInsCpx = false;
+    boolean hasThes = false;
+    boolean hasBooks = false;
+    boolean hasRef = true;
+    boolean hasEasy = true;
+    boolean hasTag = true;
 
-        boolean hasInsCpx = false;
-        boolean hasThes = false;
-        boolean hasBooks = false;
-		boolean hasRef = true;
-		boolean hasEasy = true;
-		boolean hasTag = true;
-
-		String ppd = null;
+    String ppd = null;
 
 
-        for(int i=0; i<customizedFeatures.length; ++i)
+    for(int i=0; i<customizedFeatures.length; ++i)
+    {
+        if(customizedFeatures[i].equalsIgnoreCase("CPX") ||
+           customizedFeatures[i].equalsIgnoreCase("INS"))
         {
-            if(customizedFeatures[i].equalsIgnoreCase("CPX") ||
-               customizedFeatures[i].equalsIgnoreCase("INS"))
-            {
-                hasInsCpx = true;
-            }
-
-            if(customizedFeatures[i].equalsIgnoreCase("THS"))
-            {
-                hasThes = true;
-            }
-
-            if(customizedFeatures[i].equalsIgnoreCase("ELE") ||
-               customizedFeatures[i].equalsIgnoreCase("MAT") ||
-               customizedFeatures[i].equalsIgnoreCase("CHE") ||
-               customizedFeatures[i].equalsIgnoreCase("CIV") ||
-               customizedFeatures[i].equalsIgnoreCase("COM") ||
-               customizedFeatures[i].equalsIgnoreCase("SEC"))
-            {
-                 hasBooks = true;
-            }
-
-            if(customizedFeatures[i].equalsIgnoreCase("REF"))
-            {
-				hasRef = false;
-			}
-
-			if(customizedFeatures[i].equalsIgnoreCase("EZY"))
-			{
-				hasEasy = false;
-			}
-
-			if(customizedFeatures[i].equalsIgnoreCase("TAG"))
-			{
-				hasTag = false;
-			}
-
-			if(customizedFeatures[i].indexOf("PPD") > -1)
-			{
-				ppd = customizedFeatures[i];
-			}
+            hasInsCpx = true;
         }
+
+        if(customizedFeatures[i].equalsIgnoreCase("THS"))
+        {
+            hasThes = true;
+        }
+
+        ReferexCollection[] allcols = ReferexCollection.allcolls;
+        for(int idx = 0; idx < allcols.length; idx++)
+        {
+          if(customizedFeatures[i].equalsIgnoreCase(allcols[idx].getAbbrev()))
+          {
+               hasBooks = true;
+               break;
+          }
+        }
+
+        if(customizedFeatures[i].equalsIgnoreCase("REF"))
+        {
+  	      hasRef = false;
+        }
+
+  			if(customizedFeatures[i].equalsIgnoreCase("EZY"))
+  			{
+  				hasEasy = false;
+  			}
+
+  			if(customizedFeatures[i].equalsIgnoreCase("TAG"))
+  			{
+  				hasTag = false;
+  			}
+
+  			if(customizedFeatures[i].indexOf("PPD") > -1)
+  			{
+  				ppd = customizedFeatures[i];
+  			}
+    }
 
 		if(ppd != null)
 		{
-			strBufGlobalLinksXML.append("<GLOBAL-MESSAGE>");
-			strBufGlobalLinksXML.append(ppd);
-			strBufGlobalLinksXML.append("</GLOBAL-MESSAGE>");
+  			strBufGlobalLinksXML.append("<GLOBAL-MESSAGE>");
+  			strBufGlobalLinksXML.append(ppd);
+  			strBufGlobalLinksXML.append("</GLOBAL-MESSAGE>");
 		}
 		strBufGlobalLinksXML.append("<GLOBAL-LINKS>");
 		// UI features dependent on user settings
 		if(hasTag)
 		{
-        	strBufGlobalLinksXML.append("<TAGGROUPS/>");
+      	strBufGlobalLinksXML.append("<TAGGROUPS/>");
 		}
 		// UI features dependent on user settings
 		if(hasEasy)
@@ -102,24 +101,23 @@ public class GlobalLinks {
 			strBufGlobalLinksXML.append("<THESAURUS/>");
 		}
 
-        // UI features dependent on user settings
-        if(hasBooks)
-        {
-            strBufGlobalLinksXML.append("<BOOK/>");
-        }
-
-        if(hasRef)
-        {
-			strBufGlobalLinksXML.append("<REFERENCE/>");
-		}
-
-		// rest of UI features are hardcoded defaults
-        strBufGlobalLinksXML.append("<HELP/>");
-        strBufGlobalLinksXML.append("</GLOBAL-LINKS>");
-
-        return strBufGlobalLinksXML.toString();
+    // UI features dependent on user settings
+    if(hasBooks)
+    {
+        strBufGlobalLinksXML.append("<BOOK/>");
     }
 
+    if(hasRef)
+    {
+	    strBufGlobalLinksXML.append("<REFERENCE/>");
+    }
+
+    // rest of UI features are hardcoded defaults
+    strBufGlobalLinksXML.append("<HELP/>");
+    strBufGlobalLinksXML.append("</GLOBAL-LINKS>");
+
+    return strBufGlobalLinksXML.toString();
+  }
 
 }
 
