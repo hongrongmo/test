032d89a81db4d287d89521a3d699884bad33a893
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2007-09-27 15:51:46 +0000

    changes from Miami
    mostly for handling ISBN 13's
    
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@4077 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/domain/ISBN.java
+++ eijava/eilib/src/org/ei/domain/ISBN.java
@@ -6,10 +6,10 @@ import java.io.*;
 public class ISBN
 	implements ElementData
 {
-
+  public static final String ISBN13_PREFIX = "978";
 	protected String isbn;
 	protected Key key;
-	protected boolean labels = false;
+	protected boolean labels = true;
 
 	public ISBN(String isbn)
 	{
@@ -19,10 +19,31 @@ public class ISBN
 
 	public ISBN(Key key, String isbn)
 	{
-		this.key = Keys.ISBN;
+		this.key = key;
 		this.isbn = isbn;
 	}
 
+
+  private static char getISBN13CheckDigit(String isbn) {
+    int len = isbn.length();
+    int digitSum = 0;
+    int calcValue = 0;
+
+    // length of passed in isbn must be 12 or
+    // we will get a nullpointer here
+    for (int i = 0; i < 12; i++) {
+        int val = Integer.parseInt((isbn.substring(i, i + 1)));
+        if (i % 2 == 1) {
+            digitSum += val * 3;
+        } else {
+            digitSum += val;
+        }
+    }
+
+    calcValue = (10 - (digitSum % 10)) % 10;
+
+    return (char) (calcValue + '0');
+  }
 
 	public String withoutDash()
 	{
@@ -53,14 +74,14 @@ public class ISBN
 	public void toXML(Writer out)
 		throws IOException
 	{
+
 		out.write("<");
 		out.write(this.key.getKey());
-		if(this.labels && (this.key.getLabel() != null))
-		{
+
 			out.write(" label=\"");
 			out.write(this.key.getLabel());
 			out.write("\"");
-		}
+
 		out.write("><![CDATA[");
 		out.write(this.isbn);
 		out.write("]]></");
