0b7cfe18079ee4fa9fb8cb0889ad304cb2be5463
tsolovye <tsolovye@aa8a525f-4958-0410-8862-b993faadc08d>
2007-09-28 20:06:42 +0000

    moved inner class to upper directory
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@4117 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/data/encompasslit/loadtime/EltCombiner.java
+++ eijava/eilib/src/org/ei/data/encompasslit/loadtime/EltCombiner.java
@@ -669,53 +669,5 @@ public class EltCombiner extends Combiner {
 
     }
     
-
-    public class QualifierFacet
-    {
-    	private boolean norole;
-    	private boolean reagent;
-    	private boolean product;
-    	
-    	public void setNorole(String term)
-    	{
-    		if (!this.norole && term != null && !term.equals(""))
-    		{
-    			this.norole = true;
-    		}    		
-    	}
-    	public void setReagent(String term)
-    	{
-    		if (!this.reagent && term != null && !term.equals(""))
-    		{
-    			this.reagent = true;
-    		}
-    	}
-    	public void setProduct(String term)
-    	{
-    		if (!this.product && term != null && !term.equals(""))
-    		{
-    			this.product = true;
-    		}
-    	}
-    	
-    	public String getValue()
-    	{
-    		StringBuffer value = new StringBuffer();
-    		if(norole)
-    		{
-    			value.append("N").append(";");
-    		}
-    		if(reagent)
-    		{
-    			value.append("R").append(";");
-    		}
-    		if(product)
-    		{
-    			value.append("P").append(";");
-    		}
-    		
-    		return value.toString();    			
-    	}   	
-    }
       
 }
