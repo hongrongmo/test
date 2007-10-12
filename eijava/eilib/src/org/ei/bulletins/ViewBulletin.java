4c413e6a1c6dae9fa3b0287618adfc49712a416a
hmo <hmo@aa8a525f-4958-0410-8862-b993faadc08d>
2007-10-12 19:07:58 +0000

    modify
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@4170 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/bulletins/ViewBulletin.java
+++ eijava/eilib/src/org/ei/bulletins/ViewBulletin.java
@@ -21,6 +21,7 @@ import java.util.*;
 import java.net.*;
 import java.io.*;
 import org.ei.logging.*;
+import org.ei.controller.*;
 
 
 public class ViewBulletin extends HttpServlet
@@ -31,11 +32,12 @@ public class ViewBulletin extends HttpServlet
 	String appName;
 	String authURL;
     ControllerClient client;
+    ServletConfig config;
 
     public void init()
         throws ServletException
     {
-        ServletConfig config = getServletConfig();
+        config = getServletConfig();
 		authURL = config.getInitParameter("authURL");
 		bulletinFileLocation = config.getInitParameter("bulletinFileLocation");
 		logURL = config.getInitParameter("logURL");
@@ -101,130 +103,137 @@ public class ViewBulletin extends HttpServlet
 			UserSession ussession = sCache.getUserSession(sesID, ipaddress, referrerURL, username, password,entryToken);
 
 			String status = ussession.getStatus();
+
 			if(status.equals(SessionStatus.NEW_HAD_EXPIRED))
 			{
-				ussession = sCache.getUserSession(null, ipaddress, referrerURL, username, password,entryToken);
-				status = ussession.getStatus();
-			}
-
-			User user = ussession.getUser();
-
-			String cartridges[] = user.getCartridge();
-			StringBuffer buffCartridges = new StringBuffer();
-
-			for (int i = 0; i < cartridges.length; i++)
-			{
-				buffCartridges.append(cartridges[i]);
-
-				if(i > 0)
-					buffCartridges.append(";");
-			}
-			String lcCartridges = buffCartridges.toString().toLowerCase();
-			if (lcCartridges.indexOf("lit_htm") == -1 && lcCartridges.indexOf("lit_pdf") == -1
-			&& lcCartridges.indexOf("pat_htm") == -1 && lcCartridges.indexOf("pat_pdf") == -1 )
-			{
-					throw new SecurityException("<DISPLAY>You do not have access to the bulletins.</DISPLAY>");
-			}
-
-			String id = request.getParameter("id");
-			String cType = request.getParameter("cType");
-
-			BulletinBuilder builder = new BulletinBuilder();
-
-			BulletinLinkBuilder linkBuilder = new BulletinLinkBuilder();
-
-			Bulletin bulletin = builder.buildBulletinDetail(id);
-
-			logProperties.put("EISESSION", sesID.toString());
-			logProperties.put("request", "viewBulletin");
-			logProperties.put("dbname", bulletin.getDatabase());
-			logProperties.put("category",bulletin.getCategory());
-			logProperties.put("filename", bulletin.getFileName());
-			logProperties.put("custid", user.getCustomerID());
-			logProperties.put("ctype",cType);
-
-			bulletin.setContentType(cType);
-			StringBuffer link = new StringBuffer(linkBuilder.buildLink(bulletin));
-
-			if(cType.equals("HTML"))
-			{
-				response.setContentType("text/html");
-			}
-			else if (cType.equals("PDF"))
-			{
-				response.setContentType("application/pdf");
+				String serverName = request.getServerName();
+				int serverPort = request.getServerPort();
+				if(serverPort != 80)
+				{
+					serverName = serverName+":"+Integer.toString(serverPort);
 				}
-			else if (cType.equals("ZIP"))
-			{
-				response.setContentType("application/zip");
-				StringBuffer bufHeader = new StringBuffer("attachments; ");
-				bufHeader.append("filename=").append(bulletin.getFileName()).append(".zip");
-				response.setHeader("Content-disposition", bufHeader.toString());
-			}
-			else if(cType.equals("SAVEHTML"))
-			{
-				response.setContentType("text/html");
-				StringBuffer bufHeader = new StringBuffer("attachments; ");
-				bufHeader.append("filename=").append(bulletin.getFileName()).append(".htm");
-				response.setHeader("Content-disposition", bufHeader.toString());
-			}
-			else if (cType.equals("SAVEPDF"))
-			{
-				response.setContentType("application/pdf");
-				StringBuffer bufHeader = new StringBuffer("attachments; ");
-				bufHeader.append("filename=").append(bulletin.getFileName()).append(".pdf");
-				response.setHeader("Content-disposition", bufHeader.toString());
-			}
-
-
-
-			StringBuffer fullUrl = new StringBuffer();
-			String fullLink = fullUrl.append(bulletinFileLocation).append("/").append(link).toString();
-
-			bulletinFile = new File(fullLink);
-
-			bis = new BufferedInputStream(new FileInputStream(bulletinFile));
-			bout = new BufferedOutputStream(response.getOutputStream());
-
-			byte[] buff = new byte[1024];
-			int bytesRead;
-
-			while(-1 != (bytesRead = bis.read(buff, 0, buff.length)))
-			{
-				bout.write(buff, 0, bytesRead);
-			}
-			bis.close();
-			bout.close();
-
-			//Log it
-
-			long start = System.currentTimeMillis();
-			logClient.reset();
-			logClient.setHost(request.getRemoteAddr());
-			logClient.setrfc931("-");
-			logClient.setusername("-");
-			logClient.setcust_id((Long.valueOf((String)logProperties.get("custid"))).longValue());
-			logClient.setuser_agent(request.getHeader("User-Agent"));
-			logClient.setHTTPmethod(request.getMethod());
-			logClient.setreferrer(request.getHeader("referer"));
-			logClient.seturi_stem(request.getRequestURI());
-			logClient.seturi_query(request.getQueryString());
-			logClient.setstatuscode(HttpServletResponse.SC_OK);
-			logClient.setrid(new GUID().toString());
-			logClient.setappid(appName);
-			logClient.setappdata(logProperties);
-			if(logProperties.containsKey("EISESSION"))
-			{
-				logClient.setsid((String) logProperties.get("EISESSION"));
+				response.sendRedirect("http://"+serverName+"/controller/servlet/Controller?SYSTEM_LOGOUT=true&CID="+Controller.REDIR_PAGE_SESSION_EXPIRED);
 			}
 			else
 			{
-				logClient.setsid("0");
+
+				User user = ussession.getUser();
+
+				String cartridges[] = user.getCartridge();
+				StringBuffer buffCartridges = new StringBuffer();
+
+				for (int i = 0; i < cartridges.length; i++)
+				{
+					buffCartridges.append(cartridges[i]);
+
+					if(i > 0)
+						buffCartridges.append(";");
+				}
+				String lcCartridges = buffCartridges.toString().toLowerCase();
+				if (lcCartridges.indexOf("lit_htm") == -1 && lcCartridges.indexOf("lit_pdf") == -1
+				&& lcCartridges.indexOf("pat_htm") == -1 && lcCartridges.indexOf("pat_pdf") == -1 )
+				{
+						throw new SecurityException("<DISPLAY>You do not have access to the bulletins.</DISPLAY>");
+				}
+
+				String id = request.getParameter("id");
+				String cType = request.getParameter("cType");
+
+				BulletinBuilder builder = new BulletinBuilder();
+
+				BulletinLinkBuilder linkBuilder = new BulletinLinkBuilder();
+
+				Bulletin bulletin = builder.buildBulletinDetail(id);
+
+				logProperties.put("EISESSION", sesID.toString());
+				logProperties.put("request", "viewBulletin");
+				logProperties.put("dbname", bulletin.getDatabase());
+				logProperties.put("category",bulletin.getCategory());
+				logProperties.put("filename", bulletin.getFileName());
+				logProperties.put("custid", user.getCustomerID());
+				logProperties.put("ctype",cType);
+
+				bulletin.setContentType(cType);
+				StringBuffer link = new StringBuffer(linkBuilder.buildLink(bulletin));
+
+				if(cType.equals("HTML"))
+				{
+					response.setContentType("text/html");
+				}
+				else if (cType.equals("PDF"))
+				{
+					response.setContentType("application/pdf");
+					}
+				else if (cType.equals("ZIP"))
+				{
+					response.setContentType("application/zip");
+					StringBuffer bufHeader = new StringBuffer("attachments; ");
+					bufHeader.append("filename=").append(bulletin.getFileName()).append(".zip");
+					response.setHeader("Content-disposition", bufHeader.toString());
+				}
+				else if(cType.equals("SAVEHTML"))
+				{
+					response.setContentType("text/html");
+					StringBuffer bufHeader = new StringBuffer("attachments; ");
+					bufHeader.append("filename=").append(bulletin.getFileName()).append(".htm");
+					response.setHeader("Content-disposition", bufHeader.toString());
+				}
+				else if (cType.equals("SAVEPDF"))
+				{
+					response.setContentType("application/pdf");
+					StringBuffer bufHeader = new StringBuffer("attachments; ");
+					bufHeader.append("filename=").append(bulletin.getFileName()).append(".pdf");
+					response.setHeader("Content-disposition", bufHeader.toString());
+				}
+
+				StringBuffer fullUrl = new StringBuffer();
+				String fullLink = fullUrl.append(bulletinFileLocation).append("/").append(link).toString();
+
+				bulletinFile = new File(fullLink);
+
+				bis = new BufferedInputStream(new FileInputStream(bulletinFile));
+				bout = new BufferedOutputStream(response.getOutputStream());
+
+				byte[] buff = new byte[1024];
+				int bytesRead;
+
+				while(-1 != (bytesRead = bis.read(buff, 0, buff.length)))
+				{
+					bout.write(buff, 0, bytesRead);
+				}
+				bis.close();
+				bout.close();
+
+				//Log it
+
+				long start = System.currentTimeMillis();
+				logClient.reset();
+				logClient.setHost(request.getRemoteAddr());
+				logClient.setrfc931("-");
+				logClient.setusername("-");
+				logClient.setcust_id((Long.valueOf((String)logProperties.get("custid"))).longValue());
+				logClient.setuser_agent(request.getHeader("User-Agent"));
+				logClient.setHTTPmethod(request.getMethod());
+				logClient.setreferrer(request.getHeader("referer"));
+				logClient.seturi_stem(request.getRequestURI());
+				logClient.seturi_query(request.getQueryString());
+				logClient.setstatuscode(HttpServletResponse.SC_OK);
+				logClient.setrid(new GUID().toString());
+				logClient.setappid(appName);
+				logClient.setappdata(logProperties);
+				if(logProperties.containsKey("EISESSION"))
+				{
+					logClient.setsid((String) logProperties.get("EISESSION"));
+				}
+				else
+				{
+					logClient.setsid("0");
+				}
+				long end = System.currentTimeMillis();
+				logClient.setend_time(end);
+				logClient.setresponse_time(end - start);
+				logClient.sendit();
 			}
-			long end = System.currentTimeMillis();
-			logClient.setend_time(end);
-			logClient.setresponse_time(end - start);
-			logClient.sendit();
 		}
 		catch(Exception e)
 		{
