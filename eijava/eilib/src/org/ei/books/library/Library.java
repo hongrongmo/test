052fe55c31d60e61037598e9a41f7ed995d5a064
Joel <Joel@aa8a525f-4958-0410-8862-b993faadc08d>
2007-11-01 13:58:08 +0000

    Now in sync with miami
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@4193 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/books/library/Library.java
+++ eijava/eilib/src/org/ei/books/library/Library.java
@@ -1,32 +1,44 @@
 package org.ei.books.library;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class Library extends LibraryComponent  {
 
 	private static Library uniqueInstance;
-	ArrayList libraryComponents = new ArrayList();
-	public static HashMap isbnTitleMap = new HashMap();
-	static String[] collectionNames = {"CHE","CIV","COM","ELE","MAT","SEC"};
-	private Library() { }
+	private ArrayList libraryComponents = new ArrayList();
+	private Map isbnTitleMap;
+	private static String[] collectionNames = {"CHE","CIV","COM","ELE","MAT","SEC"};
+
+
+
+
+	private Library()
+		throws Exception
+	{
+		this.isbnTitleMap = new HashMap();
+		for(int i = 0; i < collectionNames.length;i++)
+		{
+			BookCollection bookCollection = new BookCollection(collectionNames[i],
+			                                                   this.isbnTitleMap);
+			add(bookCollection);
+		}
+	}
 
 	public static synchronized Library getInstance() throws Exception
 	{
 		if(uniqueInstance == null)
 		{
 			uniqueInstance = new Library();
-
-			for(int i = 0; i < collectionNames.length;i++)
-			{
-				BookCollection bookCollection = new BookCollection(collectionNames[i]);
-				bookCollection.populateBooks();
-				uniqueInstance.add(bookCollection);
-			}
 		}
 
 		return uniqueInstance;
 	}
+
+	public Book getBook(String key)
+	{
+		return (Book) this.isbnTitleMap.get(key);
+	}
+
 	public void add(LibraryComponent libraryComponent)
 	{
 		libraryComponents.add(libraryComponent);
