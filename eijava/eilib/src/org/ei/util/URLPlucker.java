3879e56e2b21cf20e3dc8502f6169c276d1ae0b1
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2007-11-08 19:37:16 +0000

    added email addresses
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@4241 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/util/URLPlucker.java
+++ eijava/eilib/src/org/ei/util/URLPlucker.java
@@ -6,8 +6,8 @@ public class URLPlucker {
     /* Mastering Regualr Expressions 3rd Edition, Chapter 5, page 209 */
     /* Practical Regex Techniques, Extended Examples: Building Up a Regex Through Variables in Java */
 
-    private static String SubDomain  = "(?i:[a-z0-9]|[a-z0-9][-a-z0-9]*[a-z0-9])";
-    private static String TopDomains = "(?x-i:com\\b     \n" +
+    private static String SubDomain  = "[a-z0-9](?:[-a-z0-9]*[a-z0-9])?";
+    private static String TopDomains = "(?-i: com\\b     \n" +
                                       " |edu\\b         \n" +
                                       " |biz\\b         \n" +
                                       " |in(?:t|fo)\\b  \n" +
@@ -17,27 +17,29 @@ public class URLPlucker {
                                       " |[a-z][a-z]\\b  \n" + // country codes
                                       ")                \n";
 
-    private static String Hostname   = "(?:" + SubDomain + "\\.)+" + TopDomains;
+    private static String Hostname   = "(?i:" + SubDomain + "\\.)+" + TopDomains;
 
     private static String NOT_IN     = ";\"’<>()\\[\\]\\{\\}\\s\\x7F-\\xFF";
     private static String NOT_END    = "!.,?";
     private static String ANYWHERE   = "[^" + NOT_IN + NOT_END + "]";
     private static String EMBEDDED   = "[" + NOT_END + "]";
-    private static String UrlPath    = "/"+ANYWHERE + "*("+EMBEDDED+"+"+ANYWHERE+"+)*";
+    private static String UrlPath    = "/"+ANYWHERE + "*(?:"+EMBEDDED+"+"+ANYWHERE+"+)*";
     private static String Url =
                 "(?x:                                             \n"+
                 " \\b                                             \n"+
-                " ## match the hostname part                      \n"+
+                " # match the hostname part                       \n"+
                 " (                                               \n"+
-                " (?: ftp | http s? ): // [-\\w]+(\\.\\w[-\\w]*)+ \n"+
+                " (?: ftp|https? ): // [-\\w]+(?: \\.\\w[-\\w]*)+ \n"+
                 " |                                               \n"+
                 " " + Hostname + "                                \n"+
+                " |                                               \n"+
+                " (?: \\w[-.\\w]*)@" + Hostname + "               \n"+
                 " )                                               \n"+
                 " # allow optional port                           \n"+
-                " (?: \\d+ )?                                     \n"+
+                " ( : \\d+ )?                                     \n"+
                 "                                                 \n"+
                 " # rest of url is optional, and begins with /    \n"+
-                " (?: " + UrlPath + ")?                           \n"+
+                " ( " + UrlPath + ")?                             \n"+
                 ")";
 
     // Now convert string we’ve built up into a real regex object
