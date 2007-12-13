3b0bbfcfa97bc637de358a5a6d33df66f074a495
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2007-12-13 18:02:32 +0000

    java 1.4
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@4596 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/ei.bat
+++ eijava/ei.bat
@@ -1,5 +1,6 @@
 set CATALINA_HOME=c:\baja\appserver.4
-set JAVA_HOME=C:\Program Files\Java\jdk1.5.0_06
+REM set JAVA_HOME=C:\Program Files\Java\jdk1.5.0_06
+set JAVA_HOME=C:\j2sdk1.4.2_11
 
 REM %CATALINA_HOME%\bin\startup
 
