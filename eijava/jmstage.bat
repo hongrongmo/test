4493281833b268df8ba92b3e3a1f9acbc3e8c050
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2007-12-13 18:02:08 +0000

    docview copy to row2
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@4595 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/jmstage.bat
+++ eijava/jmstage.bat
@@ -32,7 +32,7 @@ mkdir "%USERPROFILE%"\Desktop\build\row2
 copy engvillage\stage\engvillage.war "%USERPROFILE%"\Desktop\build\row2
 copy engresources\stage\engresources.war "%USERPROFILE%"\Desktop\build\row2
 copy controller\stage\controller.war "%USERPROFILE%"\Desktop\build\row2
-copy docview\stage\docview.war "%USERPROFILE%"\Desktop\build\row1
+copy docview\stage\docview.war "%USERPROFILE%"\Desktop\build\row2
 
 cmd.exe /C ant clean
 
