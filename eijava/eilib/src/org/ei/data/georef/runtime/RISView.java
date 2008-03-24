bd95a40ea58a1ad9bfb05db4864cc63c13cafff4
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2008-03-24 20:51:52 +0000

    added start page and end page
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@4994 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/data/georef/runtime/RISView.java
+++ eijava/eilib/src/org/ei/data/georef/runtime/RISView.java
@@ -3,49 +3,51 @@ package org.ei.data.georef.runtime;
 import org.ei.domain.Key;
 import org.ei.domain.Keys;
 import org.ei.domain.RIS;
-import java.sql.*;
-import java.io.*;
 
 import org.ei.domain.EIDoc;
 import org.ei.domain.DocID;
 import org.ei.domain.ElementDataMap;
 import org.ei.domain.ElementData;
+import org.ei.domain.XMLWrapper;
 
+import java.sql.*;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Collection;
+import java.util.regex.*;
 
 
-    public class RISView extends DetailedView {
+public class RISView extends DetailedView {
 
-        private Key[] keys = new Key[]{Keys.RIS_TY, // DocType
-                                        Keys.RIS_LA, // Language
-                                        Keys.RIS_N1, // Copyright
-                                        Keys.RIS_TI, // Title
-                                        Keys.RIS_T1, // Tranlated title
-                                        Keys.RIS_BT, // Monograph title
-                                        Keys.RIS_JO,  // Serial title
-                                        Keys.RIS_T3,  //  Serial title if ! a journal
-                                        Keys.RIS_AUS, // Authors
-                                        Keys.RIS_AD, // Aff
-                                        Keys.RIS_EDS, // Editors
-                                        Keys.RIS_VL, // Volume
-                                        Keys.RIS_IS, // Issue
-                                        Keys.RIS_PY,  // Year
-                                        Keys.RIS_AN, // ACCESSION_NUMBER
-                                        Keys.RIS_SP,  // Start Pg
-                                        Keys.RIS_EP,  // End Pg
-                                        Keys.RIS_SN, // ISSN
-                                        Keys.RIS_S1, // ISBN
-                                        Keys.RIS_MD, // Meeting Date?
-                                        Keys.RIS_CY,  // Conference ?
-                                        Keys.RIS_PB,  // Publisher
-                                        Keys.RIS_N2, // Abstract
-                                        Keys.RIS_KW, // Main Heading
-                                        Keys.RIS_CVS, // CV
-                                        Keys.RIS_FLS, // FL
-                                        Keys.RIS_DO, // DOI
-                                        Keys.BIB_TY };
+      private Key[] keys = new Key[]{Keys.RIS_TY, // DocType
+                                      Keys.RIS_LA, // Language
+                                      Keys.RIS_N1, // Copyright
+                                      Keys.RIS_TI, // Title
+                                      Keys.RIS_T1, // Tranlated title
+                                      Keys.RIS_BT, // Monograph title
+                                      Keys.RIS_JO,  // Serial title
+                                      Keys.RIS_T3,  //  Serial title if ! a journal
+                                      Keys.RIS_AUS, // Authors
+                                      Keys.RIS_AD, // Aff
+                                      Keys.RIS_EDS, // Editors
+                                      Keys.RIS_VL, // Volume
+                                      Keys.RIS_IS, // Issue
+                                      Keys.RIS_PY,  // Year
+                                      Keys.RIS_AN, // ACCESSION_NUMBER
+                                      Keys.RIS_SP,  // Start Pg
+                                      Keys.RIS_EP,  // End Pg
+                                      Keys.RIS_SN, // ISSN
+                                      Keys.RIS_S1, // ISBN
+                                      Keys.RIS_MD, // Meeting Date?
+                                      Keys.RIS_CY,  // Conference ?
+                                      Keys.RIS_PB,  // Publisher
+                                      Keys.RIS_N2, // Abstract
+                                      Keys.RIS_KW, // Main Heading
+                                      Keys.RIS_CVS, // CV
+                                      Keys.RIS_FLS, // FL
+                                      Keys.RIS_DO, // DOI
+                                      Keys.BIB_TY };
 
       public Key[] getKeys() {
         return super.getKeys();
@@ -94,18 +96,38 @@ import java.util.Collection;
         changeElementKey(Keys.TITLE, Keys.RIS_TI);
         changeElementKey(Keys.TITLE_TRANSLATION, Keys.RIS_T1);
         changeElementKey(Keys.MONOGRAPH_TITLE, Keys.RIS_BT);
-        //      Keys.RIS_JO,  // Serial title
-        //      Keys.RIS_T3,  //  Serial title if ! a journal
+        if(mappingcode != null)
+        {
+          if(mappingcode.equals("JOUR"))
+          {
+            changeElementKey(Keys.SERIAL_TITLE, Keys.RIS_JO);
+          }
+          else
+          {
+            changeElementKey(Keys.SERIAL_TITLE, Keys.RIS_T3);
+          }
+        }
         changeElementKey(Keys.AUTHORS, Keys.RIS_AUS);
         changeElementKey(Keys.AUTHOR_AFFS, Keys.RIS_AD);
         changeElementKey(Keys.EDITORS, Keys.RIS_EDS);
         changeElementKey(Keys.VOLUME, Keys.RIS_VL);
         changeElementKey(Keys.ISSUE, Keys.RIS_IS);
         changeElementKey(Keys.PUBLICATION_YEAR, Keys.RIS_PY);
-
         changeElementKey(Keys.ACCESSION_NUMBER, Keys.RIS_AN);
-        //      Keys.RIS_SP, // Start Page
-        //      Keys.RIS_EP, // End Pg
+
+        String pages = getPages();
+        Pattern patternPages = Pattern.compile("^(\\d+)\\D+(\\d+)$");
+        Matcher pagematch = patternPages.matcher(pages);
+        if(pagematch.find())
+        {
+          ris_ht.put(Keys.RIS_SP, new XMLWrapper(Keys.RIS_SP, pagematch.group(1)));
+          ris_ht.put(Keys.RIS_EP, new XMLWrapper(Keys.RIS_EP, pagematch.group(2)));
+        }
+        else
+        {
+          ris_ht.put(Keys.RIS_SP, new XMLWrapper(Keys.RIS_SP, pages));
+        }
+
         changeElementKey(Keys.ISSN, Keys.RIS_SN);
         changeElementKey(Keys.ISBN, Keys.RIS_S1);
         //      Keys.RIS_MD, // Meeting Date?
@@ -117,24 +139,7 @@ import java.util.Collection;
         changeElementKey(Keys.UNCONTROLLED_TERMS, Keys.RIS_FLS);
         changeElementKey(Keys.DOI, Keys.RIS_DO);
 
-        Collection keySet = ris_ht.keySet();
-        Iterator itrKeys = keySet.iterator();
-        while(itrKeys.hasNext())
-        {
-          Key key = (Key) itrKeys.next();
-          Writer w = new StringWriter();
-          ElementData eldata = ((ElementData) ris_ht.get(key));
-          if(eldata != null)
-          {
-            eldata.toXML(w);
-            System.out.println(" key: " +  key.getKey() + " == " + w.toString());
-          }
-          else
-          {
-            System.out.println(" key: " +  key.getKey() + " NO DATA");
-          }
-        }
-
+        // printElementDataXML(ris_ht);
 
         EIDoc risdoc = new EIDoc(did, ris_ht, getFormat());
         risdoc.setOutputKeys(this.keys);
@@ -152,6 +157,32 @@ import java.util.Collection;
       }
 
       return;
+    }
+
+    private void printElementDataXML(ElementDataMap ht)
+    {
+        Collection keySet = ht.keySet();
+        Iterator itrKeys = keySet.iterator();
+        while(itrKeys.hasNext())
+        {
+          Key key = (Key) itrKeys.next();
+          Writer w = new StringWriter();
+          ElementData eldata = ((ElementData) ht.get(key));
+          if(eldata != null)
+          {
+            try {
+              eldata.toXML(w);
+            }
+            catch(IOException e)
+            {
+            }
+            System.out.println(" key: " +  key.getKey() + " == " + w.toString());
+          }
+          else
+          {
+            System.out.println(" key: " +  key.getKey() + " NO DATA");
+          }
+        }
     }
 
 }
