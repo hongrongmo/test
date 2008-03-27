c237eb7ee7e6b48d43e3a538e843a28c92bfb526
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2008-03-27 18:51:16 +0000

    was missing a char on longitude substring
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@5052 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/domain/RectangleCoordinates.java
+++ eijava/eilib/src/org/ei/domain/RectangleCoordinates.java
@@ -96,8 +96,8 @@ public class RectangleCoordinates
         {
           String lat1 = String.valueOf(Long.parseLong(rectangle.group(2).substring(0,2)) * (rectangle.group(1).equals("S") ? -1 : 1));
           String lat2 = String.valueOf(Long.parseLong(rectangle.group(4).substring(0,2)) * (rectangle.group(3).equals("S") ? -1 : 1));
-          String lng1 = String.valueOf(Long.parseLong(rectangle.group(6).substring(0,2)) * (rectangle.group(5).equals("W") ? -1 : 1));
-          String lng2 = String.valueOf(Long.parseLong(rectangle.group(8).substring(0,2)) * (rectangle.group(7).equals("W") ? -1 : 1));
+          String lng1 = String.valueOf(Long.parseLong(rectangle.group(6).substring(0,3)) * (rectangle.group(5).equals("W") ? -1 : 1));
+          String lng2 = String.valueOf(Long.parseLong(rectangle.group(8).substring(0,3)) * (rectangle.group(7).equals("W") ? -1 : 1));
 
 /*          String lat1 = String.valueOf(Long.parseLong(rectangle.group(2)) * (rectangle.group(1).equals("S") ? -1 : 1) + (Long.parseLong(rectangle.group(3)) * 1/60) + (Long.parseLong(rectangle.group(4)) * 1/360));
           String lat2 = String.valueOf(Long.parseLong(rectangle.group(6)) * (rectangle.group(5).equals("S") ? -1 : 1) + (Long.parseLong(rectangle.group(7)) * 1/60) + (Long.parseLong(rectangle.group(8)) * 1/360));
