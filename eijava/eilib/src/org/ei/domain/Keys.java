791b90b8416fd4e02932f6d36bcf840a51deab95
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2008-05-08 14:15:40 +0000

    New GeoRef Keys
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@5280 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/domain/Keys.java
+++ eijava/eilib/src/org/ei/domain/Keys.java
@@ -24,6 +24,26 @@ public interface Keys
 		public static Key BOOK_SECTION_TITLE = new Key("BST", "Section title");
 		public static Key BOOK_CHAPTER_START = new Key("BSPG", "Chapter start");
 
+    /* GeoRef Keys */
+    public static final Key AFFILIATION_OTHER = new Key("OAF", "Other affiliation");
+    public static final Key ANNOTATION = new Key("ANT", "Annotation");
+    public static final Key CATEGORY = new Key("CAT", "Category");
+    public static final Key COORDINATES = new Key("CRDN", "Coordinates");
+    public static final Key DEGREE_TYPE = new Key("DEG", "Degree");
+    public static final Key GRF_URLS = new Key("GURL", "URL");
+    public static final Key HOLDING_LIBRARY = new Key("HOLD", "Holding Library");
+    public static final Key ILLUSTRATION = new Key("ILLUS", "Illustrations");
+    public static final Key LOCATIONS = new Key("LOCS", "Locations");
+    public static final Key MAP_SCALE = new Key("MPS", "Map scale");
+    public static final Key MAP_TYPE = new Key("MPT", "Map type");
+    public static final Key MERIDIAN = new Key("MRDN", "Meridian data");
+    public static final Key MULTI_ISBN = new Key("MBNS", "ISBN");
+    public static final Key RESEARCH_PROGRAM = new Key("RPGM", "Research program");
+    public static final Key SOURCE_MEDIUM = new Key("MED", "Source medium");
+    public static final Key SUMMARY_ONLY_NOTE  = new Key("SUM", "Summary only note");
+    public static final Key SOURCE_NOTE  = new Key("SRCNT", "Source note");
+    public static final Key TARGET_AUDIENCE = new Key("AUD", "Target audience");
+    public static final Key UNIVERSITY = new Key("UNV", "University");
 
     public static final Key ABBRV_MON_MONOGRAPH_TITLE  = new Key("ME", "");
     public static final Key ABBRV_SERIAL_TITLE = new Key("SE", "Abbreviated serial title");
