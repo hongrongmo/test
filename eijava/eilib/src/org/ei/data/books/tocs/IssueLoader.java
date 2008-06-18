2611e48c37704a3b843eb32cb4217ef57f26d236
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2008-06-18 20:07:42 +0000

    now extends XMLLoader  base class
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@5334 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/data/books/tocs/IssueLoader.java
+++ eijava/eilib/src/org/ei/data/books/tocs/IssueLoader.java
@@ -1,24 +1,13 @@
 package org.ei.data.books.tocs;
 
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.StringReader;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-import org.apache.commons.digester.Digester;
-import org.apache.commons.digester.xmlrules.DigesterLoader;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
-public class IssueLoader {
+public class IssueLoader extends XMLLoader {
     protected static Log log = LogFactory.getLog(IssueLoader.class);
     
     public static void main(String[] args) {
@@ -40,41 +29,15 @@ public class IssueLoader {
         rulesString.append("</pattern>");
         rulesString.append("</digester-rules>");
     }
+
     public static Issue getIssue(File issue) {
-        List<Issue> issues = new ArrayList<Issue>();
-        Issue anissue = new Issue();
-        
-        System.setProperty("javax.xml.parsers.SAXParserFactory","org.apache.xerces.jaxp.SAXParserFactoryImpl");
-        try {
-            InputSource xmlRules = new InputSource(new StringReader(rulesString.toString()));
-            InputSource xmlSource = new InputSource(new FileReader(issue.toString()));
-
-            SAXParserFactory factory = SAXParserFactory.newInstance();
-            SAXParser saxParser = factory.newSAXParser();
-            Digester digester = new Digester(saxParser);
-            
-            digester = DigesterLoader.createDigester(xmlRules, digester);
-            digester.setEntityResolver(new BookDTDEntityResolver());
-            
-            // Push empty List onto Digester's Stack
-            digester.push( issues );
-            // Parse the XML document
-            digester.parse(xmlSource);
-        } catch (SAXException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (ParserConfigurationException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        if(!issues.isEmpty()) {
-            anissue = issues.iterator().next();
-        }
-        
-        return anissue;
-    }
-
+      List items = new LinkedList();
+      Issue anissue = null;
+      digest(issue, rulesString.toString(), items);
+     
+      if(!items.isEmpty()) {
+        anissue = (Issue) items.iterator().next();
+      }        
+      return anissue;
+  }
 }
