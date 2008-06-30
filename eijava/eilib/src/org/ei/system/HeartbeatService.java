738ef0c6e083d1b738518043499bb590ce4353e3
Joel <Joel@aa8a525f-4958-0410-8862-b993faadc08d>
2008-06-30 17:35:10 +0000

    Now uses new build class
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@5362 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/system/HeartbeatService.java
+++ eijava/eilib/src/org/ei/system/HeartbeatService.java
@@ -3,7 +3,6 @@ package org.ei.system;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,18 +11,18 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HeartbeatService extends HttpServlet
 {
-	
-	private long startup; 
+
+	private long startup;
 
 	public void init()
 		throws ServletException
 	{
 		this.startup = System.currentTimeMillis();
-		
+
 	}
 
-	
-	public void service(HttpServletRequest request, 
+
+	public void service(HttpServletRequest request,
 			    HttpServletResponse response)
 		throws IOException, ServletException
 	{
@@ -31,7 +30,7 @@ public class HeartbeatService extends HttpServlet
 		Runtime rt = Runtime.getRuntime();
 		long totalMemory = rt.totalMemory();
 		long freeMemory = rt.freeMemory();
-	
+
 		PrintWriter out = response.getWriter();
 		long currentTime = System.currentTimeMillis();
 		long uptime = currentTime - startup;
@@ -39,6 +38,8 @@ public class HeartbeatService extends HttpServlet
 		out.println("<html>");
 		out.println("<head><title>HeartbeatService</title></head>");
 		out.println("<body>");
+		out.println("<br>Build: "      + Build.version());
+		out.println("<br>China: "      + Build.china());
 		out.println("<br>UPTIME: "      + Double.toString(uphours));
 		out.println("<br>TOTAL MEMORY: "+ totalMemory);
 		out.println("<br>FREE MEMORY: " + freeMemory);
