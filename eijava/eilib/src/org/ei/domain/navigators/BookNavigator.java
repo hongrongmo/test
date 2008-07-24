f3601d68fd33bab4ce887a318f21bad8365852d2
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2008-07-24 18:59:50 +0000

    added remove of ELE4, COM4,  etc. from Book Collection Navigator (2008 Frontlist codes)
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@5412 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/domain/navigators/BookNavigator.java
+++ eijava/eilib/src/org/ei/domain/navigators/BookNavigator.java
@@ -142,6 +142,7 @@ public class BookNavigator extends EiNavigator {
 	    anav.getModifiers().remove(new EiModifier(0,col.getAbbrev().concat("1"),col.getAbbrev().concat("1")));
 	    anav.getModifiers().remove(new EiModifier(0,col.getAbbrev().concat("2"),col.getAbbrev().concat("2")));
 	    anav.getModifiers().remove(new EiModifier(0,col.getAbbrev().concat("3"),col.getAbbrev().concat("3")));
+	    anav.getModifiers().remove(new EiModifier(0,col.getAbbrev().concat("4"),col.getAbbrev().concat("4")));
 	}
 
 	public static EiNavigator cleanCLNavigator(EiNavigator anav)
