ce9367b8f9c2936840a9254b6e38ba7a39d98169
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2008-07-25 12:59:30 +0000

    Consolidated classes into functionality of a single class
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@5417 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/data/books/tocs/BookCreator.java
+++ eijava/eilib/src/org/ei/data/books/tocs/BookCreator.java
@@ -1,11 +1,25 @@
 package org.ei.data.books.tocs;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.xml.transform.Result;
+import javax.xml.transform.stream.StreamResult;
+
 public class BookCreator extends ReferexBaseProcessor  {
+
+  private boolean createWholePdf = false;
+  private boolean burstWholePdf = false;
+  private boolean stampWholePdf = false;
+  private boolean copyChapters = false;
+  private boolean createToc = false;
+  private boolean checkArchive = false;
+  
+  private static final File xsltFile = new File("xsl\\BookTOC.xsl");
 
   public boolean process(String xmlpath) throws IOException {
 
@@ -18,19 +32,177 @@ public class BookCreator extends ReferexBaseProcessor  {
       }
       if (checkIsbn(isbn)) {
         log.info(isbn + ": " + xmlFile);
-
+        
+        HashMap bookInfo = getBookInfo(isbn);
+        
         List<IncludeItem> includeitems = IssueLoader.getIssue(xmlFile).getIncludeItems();
         Iterator<IncludeItem> piis = includeitems.iterator();
 
-        pdfprocessor.createPDF(xmlFile, WHOLE_PDFS + isbn + ".pdf", piis);
+        String bookpdf = WHOLE_PDFS + isbn + ".pdf";
+        String stampedbookpdf = WHOLE_PDFS + isbn + "_stamped.pdf";
+
+        if(isCheckArchive()) {
+          checkBookArchives(xmlFile, isbn);
+        }
         
-        pdfprocessor.burstPDF(new File(WHOLE_PDFS + isbn + ".pdf"), BURST_AND_EXTRACTED + FILE_SEP + isbn);
-        pdfprocessor.stampPDF(new File(WHOLE_PDFS + isbn + ".pdf"),
-            new File(WHOLE_PDFS + isbn + "_stamped.pdf"),
-            getBookInfo(isbn));
+        if(isCreateToc()) {
+          createToc(xmlFile, isbn);
+        }
+        if(isCreateWholePdf()) {
+          pdfprocessor.createPDF(xmlFile, bookpdf, piis);
+        }
+        
+        if(isBurstWholePdf()) {
+          pdfprocessor.burstPDF(new File(bookpdf), BURST_AND_EXTRACTED + FILE_SEP + isbn);
+        }
+
+        if(isStampWholePdf())
+        {
+          pdfprocessor.stampPDF(new File(bookpdf),
+              new File(stampedbookpdf),
+              bookInfo);
+
+          if(new File(bookpdf).delete())
+          {
+            new File(stampedbookpdf).renameTo(new File(bookpdf));
+          }
+        }
+        
+        if(isCopyChapters())
+        {
+          // reset iterator
+          piis = includeitems.iterator();
+
+          while (piis.hasNext()) {
+            IncludeItem pii = piis.next();
+            String foldername = pii.getPii().replaceAll("\\p{Punct}", "");
+  
+            pdfprocessor.copyChapter(xmlFile, isbn, foldername, BURST_AND_EXTRACTED);
+            String chapterpdf = BURST_AND_EXTRACTED + isbn + FILE_SEP + foldername + ".pdf";
+            String stampedchapterpdf = BURST_AND_EXTRACTED + isbn + FILE_SEP + foldername + "_stamped.pdf";
+            pdfprocessor.stampPDF(new File(chapterpdf),
+                new File(stampedchapterpdf),
+                bookInfo);
+  
+              if(new File(chapterpdf).delete())
+              {
+                new File(stampedchapterpdf).renameTo(new File(chapterpdf));
+              }
+          }
+        }
         result = true;
       }
     }
     return result;
   }
+  
+  private boolean createToc(File xmlFile, String isbn) {
+    boolean result = false;
+  
+    Issue iss = IssueLoader.getIssue(xmlFile);
+    if(iss != null) {
+      try {
+        File burst = new File(BURST_AND_EXTRACTED + isbn); 
+        if(!burst.exists()) {
+          burst.mkdir();
+        }
+        if(burst.isDirectory()) 
+        {
+          Result tresult = new StreamResult(new FileOutputStream(BURST_AND_EXTRACTED + isbn + FILE_SEP + isbn + "_toc.html"));
+          log.info(BURST_AND_EXTRACTED + isbn + FILE_SEP + isbn + "_toc.html");              
+          if(toctransformer.runTransform(xmlFile,xsltFile,tresult)) {
+            result = true;
+          }
+        }
+      } catch(IOException e) {
+          log.error(e);
+      }
+    }
+      return result;
+  }
+  
+  private boolean checkBookArchives(File xmlFile, String isbn) {
+    boolean flagged = false;
+
+    List<IncludeItem> includeitems = IssueLoader.getIssue(xmlFile).getIncludeItems();
+    Iterator<IncludeItem> piis = includeitems.iterator();
+    int mainxmlpagetotal = 0;
+    int pdfpagetotal = 0;
+    while(piis.hasNext()) {
+      IncludeItem item = piis.next();
+      List<PageRange> pageRanges = item.getPageRanges();
+      Iterator<PageRange> ranges = pageRanges.iterator();
+      int xmlpages = 0;
+      while(ranges.hasNext())
+      {
+        xmlpages += ranges.next().length(); 
+        mainxmlpagetotal += xmlpages;
+      }
+      int pdfpages = 0;
+      try {
+        pdfpages = pdfprocessor.countPages(xmlFile, item);
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+      pdfpagetotal += pdfpages;
+      if(pdfpages != xmlpages) {
+        log.error(isbn + " " + item.getPii() + " main.xml length: " + xmlpages + " Chapter main.pdf pages " + pdfpages);
+        flagged = true;
+      }
+    }
+    if (flagged) {
+      log.error(isbn + " " + " main.xml total: " + mainxmlpagetotal + ". Chapter main.pdf totals: " + pdfpagetotal);
+    }
+    
+    log.info(isbn + ": passes checks successfully.");
+    return flagged;
+  }
+  
+  public boolean isBurstWholePdf() {
+    return burstWholePdf;
+  }
+
+  public void setBurstWholePdf(boolean burstWholePdf) {
+    this.burstWholePdf = burstWholePdf;
+  }
+
+  public boolean isCopyChapters() {
+    return copyChapters;
+  }
+
+  public void setCopyChapters(boolean copyChapters) {
+    this.copyChapters = copyChapters;
+  }
+
+  public boolean isCreateWholePdf() {
+    return createWholePdf;
+  }
+
+  public void setCreateWholePdf(boolean createWholePdf) {
+    this.createWholePdf = createWholePdf;
+  }
+
+  public boolean isStampWholePdf() {
+    return stampWholePdf;
+  }
+
+  public void setStampWholePdf(boolean stampWholePdf) {
+    this.stampWholePdf = stampWholePdf;
+  }
+  public boolean isCreateToc() {
+    return createToc;
+  }
+  public void setCreateToc(boolean createToc) {
+    this.createToc = createToc;
+  }
+
+  public boolean isCheckArchive() {
+    return checkArchive;
+  }
+
+  public void setCheckArchive(boolean checkArchives) {
+    this.checkArchive = checkArchives;
+  }
+  
 }
