0aa7d5383d239ae8566e04f7cd9ab9244a25f2ba
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2008-07-25 19:58:36 +0000

    Just like the bookCreator, this class now encompasses all the functions of create, burst, stamp, createToc, etc.
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@5423 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/data/books/tocs/BookSeriesCreator.java
+++ eijava/eilib/src/org/ei/data/books/tocs/BookSeriesCreator.java
@@ -1,54 +1,116 @@
 package org.ei.data.books.tocs;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.xml.transform.Result;
+import javax.xml.transform.stream.StreamResult;
+
 public class BookSeriesCreator extends ReferexBaseProcessor {
+  private static final File xsltFile = new File("xsl\\BooKSeriesTOC.xsl");
 
   public boolean process(String xmlpath) throws IOException {
     
-    PdfProcessorStamper processor = new PdfProcessorStamper();
     boolean result = false;
     File xmlFile = new File(xmlpath + FILE_SEP + "issue.xml");
     Issue anissue = IssueLoader.getIssue(xmlFile);
     if (anissue != null) {
       String issn = cleanIdentifier(anissue.getIssn());
       String isbn = cleanIdentifier(anissue.getIsbn());
-      if ((isbn != null) && checkIsbn(isbn)) {
+      if ((isbn != null)) {
         if (isbn.length() == 10) {
           isbn = convertToIsbn13(isbn);
         }
-        log.info(isbn + "/" + issn + ": " + xmlFile);
+        if(checkIsbn(isbn)) {
+          log.info(isbn + "/" + issn + ": " + xmlFile);
 
-        List<IncludeItem> issuearticles = anissue.getIncludeItems();
-        Iterator<IncludeItem> includeitems = issuearticles.iterator();
+          HashMap bookInfo = getBookInfo(isbn);
 
-        while (includeitems.hasNext()) {
-          IncludeItem includeitem = includeitems.next();
-          String pii = cleanIdentifier(includeitem.getPii());
-          String mainxmlfolder = pii.replace("S" + issn, "");
-          // article file is in folder which is article pii without the
-          // "S<issn>" as the prefix
-          File mainxmlFile = new File(xmlpath
-              + FILE_SEP + mainxmlfolder
-              + FILE_SEP + "main.xml");
-          if (mainxmlFile.exists()) {
-            Article article = ArticleLoader.getArticle(mainxmlFile);
-            if (article == null) {
-              log.info(mainxmlFile);
-            } else {
-              log.debug("\t\t" + article.getTitle());
-              result = true;
-            }
-          } else {
-            log.info("\t Cannot find " + mainxmlFile + "!");
+          List<IncludeItem> issuearticles = anissue.getIncludeItems();
+          Iterator<IncludeItem> includeitems = issuearticles.iterator();
+  
+          String bookpdf = WHOLE_PDFS + isbn + ".pdf";
+          String stampedbookpdf = WHOLE_PDFS + isbn + "_stamped.pdf";
+
+          if(isCreateToc()) {
+            createToc(xmlFile, isbn);
           }
-        } // while
-        processor.burstPDF(new File(WHOLE_PDFS + isbn + ".pdf"), BURST_AND_EXTRACTED + FILE_SEP + isbn);
+          
+          if(isCreateWholePdf()) {
+            pdfprocessor.createPDF(xmlFile, bookpdf, includeitems);
+          }
+          if(isBurstWholePdf()) {
+            pdfprocessor.burstPDF(new File(bookpdf), BURST_AND_EXTRACTED + FILE_SEP + isbn);
+          }
+
+          if(isStampWholePdf())
+          {
+            pdfprocessor.stampPDF(new File(bookpdf),
+                new File(stampedbookpdf),
+                bookInfo);
+  
+            if(new File(bookpdf).delete())
+            {
+              new File(stampedbookpdf).renameTo(new File(bookpdf));
+            }
+          }
+
+          if(isCopyChapters())
+          {
+            // reset iterator
+            includeitems = issuearticles.iterator();
+  
+            while (includeitems.hasNext()) {
+              IncludeItem includeitem = includeitems.next();
+              String foldername = cleanIdentifier(includeitem.getPii());
+  
+              pdfprocessor.copyChapter(xmlFile, isbn, foldername, BURST_AND_EXTRACTED);
+    
+              String chapterpdf = BURST_AND_EXTRACTED + isbn + FILE_SEP + foldername + ".pdf";
+              String stampedchapterpdf = BURST_AND_EXTRACTED + isbn + FILE_SEP + foldername + "_stamped.pdf";
+              pdfprocessor.stampPDF(new File(chapterpdf),
+                  new File(stampedchapterpdf),
+                  bookInfo);
+    
+                if(new File(chapterpdf).delete())
+                {
+                  new File(stampedchapterpdf).renameTo(new File(chapterpdf));
+                }
+            } // while
+          }
+          result = true;
+        }
       }
     }
     return result;
   }
+  private boolean createToc(File xmlFile, String isbn) {
+    boolean result = false;
+  
+    Issue iss = IssueLoader.getIssue(xmlFile);
+    if(iss != null) {    
+      try {
+        File burst = new File(BURST_AND_EXTRACTED + isbn); 
+        if(!burst.exists()) {
+          burst.mkdir();
+        }
+        if(burst.isDirectory()) 
+        {
+          Result tresult = new StreamResult(new FileOutputStream(BURST_AND_EXTRACTED + isbn + FILE_SEP + isbn + "_toc.html"));
+          log.info(BURST_AND_EXTRACTED + isbn + FILE_SEP + isbn + "_toc.html");              
+          if(toctransformer.runTransform(xmlFile,xsltFile,tresult)) {
+            result = true;
+          }
+        }
+      } catch(IOException e) {
+          log.error(e);
+      }
+    }    
+    return result;
+  }  
+  
 }
