0e4fa96f1c18b90825ffad940e896eb574af5ed3
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2008-07-25 13:01:10 +0000

    consolidate cook creation functions into single BookXCreator class with methods to set functionality
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@5419 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/data/books/tocs/ReferexRunner.java
+++ eijava/eilib/src/org/ei/data/books/tocs/ReferexRunner.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
@@ -17,50 +18,69 @@ public class ReferexRunner {
   
   public static void main(String[] args) {
     try {
-      //String isbnlist = "v:\\2008frontlist.txt";
-      String isbnlist = "v:\\2007waiting.txt";
-      //String isbnlist = "v:\\threeupdates.txt";
-      
-      BufferedReader rdr = new BufferedReader(new FileReader(isbnlist));
-      while (rdr.ready()) {
-        String aline = rdr.readLine();
-        isbnList.add(aline);
+      List<String> fileList = new ArrayList<String>();
+      fileList.add( "v:\\2007waiting.txt");
+      fileList.add( "v:\\2008frontlist.txt");
+      fileList.add( "v:\\threeupdates.txt");
+      Iterator<String> fileItr = fileList.iterator();
+      while(fileItr.hasNext()) {
+        String isbnlist = fileItr.next();
+        BufferedReader rdr = new BufferedReader(new FileReader(isbnlist));
+        while (rdr.ready()) {
+          String aline = rdr.readLine();
+          isbnList.add(aline);
+        }
+        rdr.close();
       }
-      rdr.close();
     } catch (IOException ioe) {
       isbnList = new ArrayList<String>();
     }
     /* manually override isbnList here */
-//    isbnList = new ArrayList<String>();
-//    isbnList.add("9781597492171");
-
+    isbnList = new ArrayList<String>();
+    isbnList.add("9780123744906");
+    isbnList.add("9780750685399");
+    isbnList.add("9780750687034");
+    isbnList.add("9780123741110");
+    isbnList.add("9780123708694");
+    isbnList.add("9780123744005");
+    isbnList.add("9780750685191");
+    isbnList.add("9781597491174");
+    isbnList.add("9781597492409");
+    isbnList.add("9780123725783");
+    
     if (isbnList.isEmpty()) {
       log.error("ISBN list is empty! Processing ALL encountered isbns.");
     }
+    else {
+      log.info("Processing " + isbnList.size() + " isbns.");
+    }
 
-    ReferexBaseProcessor processor;
     ArchiveMapper mapper;
     int processed = 0;
     
     try {
-      processor = new BookTocTransform();
-      processor.setIsbnList(isbnList);
       mapper = new BookArchiveMapper();
-      processed = mapper.map(processor);
+
+      BookCreator bookProcessor = new BookCreator();
+      bookProcessor.setIsbnList(isbnList);
+
+      bookProcessor.setCheckArchive(true);
+//      bookProcessor.setCreateToc(true);
+//      bookProcessor.setCreateWholePdf(true);
+//      bookProcessor.setBurstWholePdf(true);
+//      bookProcessor.setCopyChapters(true);
+      
+      processed = mapper.map(bookProcessor);
       log.info("Total processed = " + processed);
 
+
+//      ReferexBaseProcessor processor;
 //      processor = new BookSeriesTocTransform();
 //      processor.setIsbnList(isbnList);
 //      mapper = new BookSeriesArchiveMapper();
 //      processed = mapper.map(processor);
 //      log.info("Total processed = " + processed);
-
-      processor = new BookCreator();
-      processor.setIsbnList(isbnList);
-      mapper = new BookArchiveMapper();
-      processed = mapper.map(processor);
-      log.info("Total processed = " + processed);
-     
+//
 //      processor = new BookSeriesCreator();
 //      processor.setIsbnList(isbnList);
 //      mapper = new BookSeriesArchiveMapper();
