cdd325cd7a3b2afef6910dfb3fa9f4c3d4474376
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2008-07-31 15:50:04 +0000

    redesigned to create and work from isbn list
    instead of being responsible for driving book process inside of loop
    
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@5441 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/data/books/tocs/ArchiveMapper.java
+++ eijava/eilib/src/org/ei/data/books/tocs/ArchiveMapper.java
@@ -1,8 +1,31 @@
 package org.ei.data.books.tocs;
 
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-public interface ArchiveMapper {
+import org.ei.data.books.tools.PDF_FileInfo;
 
-  public int map(ArchiveProcessor processor) throws IOException;
+
+public abstract class ArchiveMapper {
+
+  public abstract void createmap();
+  private Map<String, String> archiveMap = new HashMap<String, String>();
+
+  public String getArchviePath(String isbn) {
+    return archiveMap.get(isbn);
+  }
+
+  public void addArchviePath(String isbn, String archivepath) {
+    archiveMap.put(isbn, archivepath);
+  }
+  public String cleanIdentifier(String id) {
+    return (id != null) ? id.replaceAll("\\p{Punct}", "") : null;
+  }
+
+  public String convertToIsbn13(String isbn) {
+    String newisbn;
+    String isbnroot = PDF_FileInfo.BN13_PREFIX + isbn.substring(0, 9);
+    newisbn = isbnroot + PDF_FileInfo.getISBN13CheckDigit(isbnroot);
+    return newisbn;
+  }
 }
