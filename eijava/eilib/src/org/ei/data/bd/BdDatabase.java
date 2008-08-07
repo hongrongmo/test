7e7cd5ea45cfc9ce92504e88a639db14ea4a0386
tsolovye <tsolovye@aa8a525f-4958-0410-8862-b993faadc08d>
2008-08-07 14:28:24 +0000

    Initial revision.
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@5461 aa8a525f-4958-0410-8862-b993faadc08d

---
+++ eijava/eilib/src/org/ei/data/bd/BdDatabase.java
@@ -0,0 +1,37 @@
+package org.ei.data.bd;
+
+import java.util.*;
+import org.ei.data.bd.loadtime.*;
+
+public class BdDatabase
+{
+    private static final String BD = "bd";
+
+    private static final Set bddatasources = new HashSet();
+    static
+    {
+        bddatasources.add("cpx");
+        bddatasources.add("c84");
+        bddatasources.add("geo");
+        bddatasources.add("pch");
+        bddatasources.add("elt");
+    }
+
+    public boolean isBdDatabase(String dbKey)
+    {
+        if(bddatasources.contains(dbKey))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public String getID()
+    {
+        return BD;
+    }
+
+}
---
+++ eijava/eilib/src/org/ei/data/bd/BdDocumentType.java
@@ -0,0 +1,63 @@
+/*
+ * Created on Jun 16, 2008
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.ei.data.bd;
+import java.util.*;
+import org.ei.data.bd.loadtime.*;
+/**
+ * @author solovyevat
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class BdDocumentType
+{
+    private static final Hashtable bdDocType = new Hashtable();
+
+    static
+    {  	bdDocType.put("ab","JA");
+		bdDocType.put("ar","JA");
+		bdDocType.put("bk","MC");
+		bdDocType.put("br","MR");
+		bdDocType.put("bz","JA");
+		bdDocType.put("ch","MC");
+		//bdDocType.put("cp","CA");
+		bdDocType.put("cr","CP");
+		bdDocType.put("di","DS");
+		bdDocType.put("ed","JA");
+		bdDocType.put("er","JA");
+		bdDocType.put("ip","JA");
+		bdDocType.put("le","JA");
+		bdDocType.put("no","JA");
+		bdDocType.put("pa","JA");
+		bdDocType.put("pr","JA");
+		bdDocType.put("re","JA");
+		bdDocType.put("rp","RR");
+		bdDocType.put("sh","JA");
+    	bdDocType.put("wp","JA");
+	}
+
+    public static String getDocType(String doctype,
+    								boolean confCode)
+    {
+        String dt = doctype.toLowerCase().trim();
+        if(dt.equals("cp"))
+        {
+			if(!confCode)
+			{
+				return "JA";
+			}
+			else
+			{
+				return "CA";
+			}
+		}
+		else
+		{
+			return (String) bdDocType.get(dt);
+		}
+    }
+}
