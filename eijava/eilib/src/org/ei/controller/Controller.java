1ca8364c4cbcfdb065ce9437f987a25c10c2a452
Joel <Joel@aa8a525f-4958-0410-8862-b993faadc08d>
2008-09-12 18:42:02 +0000

    Enhanced code
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@5529 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/controller/Controller.java
+++ eijava/eilib/src/org/ei/controller/Controller.java
@@ -481,9 +481,9 @@ public class Controller extends HttpServlet
         			               password,
         			               entryToken);
 
-		if(!ipGood(us.getUser().getIpAddress(), ipAddress))
+		if(!ipGood(us.getUser().getIpAddress(), ipAddress, us.getUser().getCustomerID()))
 		{
-			System.out.println("Bad IP:"+us.getUser().getIpAddress());
+			System.out.println("Bad IP:"+us.getUser().getIpAddress()+" : "+ipAddress+" : "+us.getUser().getCustomerID());
 			throw new Exception("Security Violation:"+ipAddress);
 		}
 
@@ -555,9 +555,30 @@ public class Controller extends HttpServlet
     }
 
 	private boolean ipGood(String originalIP,
-						   String currentIP)
+						   String currentIP,
+						   String customerID)
 	{
-		if(originalIP.equals("-") || currentIP.equals("127.0.0.1"))
+		if(originalIP.equals("-") ||
+		   currentIP.equals("127.0.0.1") ||
+		   customerID.equals("994173") ||
+		   customerID.equals("1001870") ||
+		   customerID.equals("8922") ||
+		   customerID.equals("1000438") ||
+		   customerID.equals("1000438") ||
+		   customerID.equals("525") ||
+		   customerID.equals("1001861") ||
+		   customerID.equals("28244") ||
+		   customerID.equals("826") ||
+		   customerID.equals("4583") ||
+		   customerID.equals("1001837") ||
+		   customerID.equals("1000637") ||
+		   customerID.equals("1001827") ||
+		   customerID.equals("923101") ||
+		   customerID.equals("1000302") ||
+		   customerID.equals("1002808") ||
+ 		   customerID.equals("21002022") ||
+ 		   customerID.equals("21007498") ||
+ 		   customerID.equals("1000397"))
 		{
 			return true;
 		}
