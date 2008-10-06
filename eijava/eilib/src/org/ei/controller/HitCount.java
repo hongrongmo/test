10c9ecbf5a77b8e1867907ed09ef9f5cf4a3e642
skariam <skariam@aa8a525f-4958-0410-8862-b993faadc08d>
2008-10-06 13:12:04 +0000

    Initial revision.
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@5614 aa8a525f-4958-0410-8862-b993faadc08d

---
+++ eijava/eilib/src/org/ei/controller/HitCount.java
@@ -0,0 +1,63 @@
+package org.ei.controller;
+
+import org.ei.security.utils.SecureID;
+
+public class HitCount
+{
+	private String key;
+	private long createTime;
+	private int hits = 0;
+	private boolean blocked;
+
+
+	public HitCount(String key) throws Exception
+
+	{
+		this.key = key;
+		this.createTime = System.currentTimeMillis();
+		this.hits++;
+		this.blocked = false;
+	}
+
+
+	public HitCount(String key, String value)
+	{
+		this.key = key;
+		String [] valueArray = value.split(":");
+		this.createTime = Long.parseLong(valueArray[0]);
+		this.hits = Integer.parseInt(valueArray[1]);
+		//this.blocked = Boolean.parseBoolean(valueArray[2]);
+		this.blocked = Boolean.valueOf(valueArray[2]).booleanValue();
+		this.hits++;
+		//if(this.hits > 12)
+		if(this.hits > 2)
+		{
+			double hitsPerSecond = (((double)System.currentTimeMillis()/(double)1000) - ((double)createTime)/(double)1000)/(double)(hits);
+			//if(hitsPerSecond > 3)
+			if(hitsPerSecond > 1)
+			{
+				blocked = true;
+			}
+		}
+	}
+
+	public String getKey()
+	{
+		return this.key;
+	}
+
+	public long getCreateTime()
+	{
+		return this.createTime;
+	}
+
+	public boolean getBlocked()
+	{
+		return this.blocked;
+	}
+
+	public String stringValue()
+	{
+		return this.createTime+":"+this.hits+":"+Boolean.toString(this.blocked);
+	}
+}
