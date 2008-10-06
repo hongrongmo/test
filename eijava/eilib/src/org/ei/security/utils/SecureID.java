2384e719ee60cf28e45aa1647282b00c0ef64e94
skariam <skariam@aa8a525f-4958-0410-8862-b993faadc08d>
2008-10-06 13:08:06 +0000

    Initial revision.
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@5612 aa8a525f-4958-0410-8862-b993faadc08d

---
+++ eijava/eilib/src/org/ei/security/utils/SecureID.java
@@ -0,0 +1,69 @@
+package org.ei.security.utils;
+
+import org.ei.util.GUID;
+import org.ei.util.MD5Digester;
+
+public class SecureID
+{
+	private static final String SEED = "fkjhsd9tr87whfs3297fshjk";
+
+	public static String getSecureID(long ttl)
+		throws Exception
+	{
+		MD5Digester digester = new MD5Digester();
+		GUID guid = new GUID();
+		String guidString = guid.toString();
+		String createTime = null;
+		String timeToLive = null;
+		StringBuffer hashBuffer = new StringBuffer(guidString);
+		hashBuffer.append(SEED);
+
+		if(ttl > 0)
+		{
+			createTime = Long.toString(System.currentTimeMillis());
+			timeToLive = Long.toString(ttl);
+			hashBuffer.append(createTime).append(timeToLive);
+		}
+
+		String hashed = digester.asHex(digester.digest(hashBuffer.toString()));
+		StringBuffer buffer = new StringBuffer(guidString);
+		buffer.append("-").append(hashed);
+
+		if(ttl > 0)
+		{
+			buffer.append("-").append(createTime).append("-").append(ttl);
+		}
+		return buffer.toString();
+	}
+
+	public static boolean validSecureID(String secureID)
+		throws Exception
+	{
+		String[] parts = secureID.split("-");
+		String guid = parts[0];
+		String hash = parts[1];
+		String timestamp = null;
+		long ttl = -1L;
+		if(parts.length == 4)
+		{
+			timestamp = parts[2];
+			ttl = Long.parseLong(parts[3]);
+		}
+		StringBuffer hashBuffer = new StringBuffer(guid);
+		hashBuffer.append(SEED);
+
+		if(parts.length == 4)
+		{
+			long timediff = System.currentTimeMillis() - Long.parseLong(timestamp);
+			if(ttl < timediff)
+			{
+				return false;
+			}
+			hashBuffer.append(timestamp).append(ttl);
+		}
+
+		MD5Digester digester = new MD5Digester();
+		String hashTest = digester.asHex(digester.digest(hashBuffer.toString()));
+		return hashTest.equals(hash);
+	}
+}
