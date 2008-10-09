7da58f78aa6c334395df6db91629bd3def2cbc1f
johna <johna@aa8a525f-4958-0410-8862-b993faadc08d>
2008-10-09 17:37:44 +0000

    Updated for 2009 data
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@5687 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/domain/SearchForm.java
+++ eijava/eilib/src/org/ei/domain/SearchForm.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class SearchForm {
 
-    public static final int ENDYEAR = 2008;
+    public static final int ENDYEAR = 2009;
 
 
     public static Map getDiscipline(int selecteddbMask) {
