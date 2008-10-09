cbf140df1970b467e4ba32113acb1e254a148ab3
skariam <skariam@aa8a525f-4958-0410-8862-b993faadc08d>
2008-10-09 15:28:14 +0000

    Image rotation and color added
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@5679 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/security/captcha/Config.java
+++ eijava/eilib/src/org/ei/security/captcha/Config.java
@@ -10,7 +10,7 @@ public class Config {
     static public final String IMAGE_HEIGHT = "40";
     static public final String PASS_CODE = "n2f3r01-782108h-324908";
     static public final String ALGORITHM = "PBEWithMD5AndDES";    
-    static public final String SKEW = "0.5";
+    static public final String SKEW = "1";
     static public final String DRAW_LINES = "3";
     static public final String DRAW_BOXES = "300";
     static public final String SKEW_PROCESSOR_CLASS = "org.ei.security.captcha.SkewImageProba:org.ei.security.captcha.SkewImageSimple";
