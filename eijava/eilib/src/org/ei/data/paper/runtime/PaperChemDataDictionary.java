74d6069d6a1b23924d416c5af5c49769fcd9ec58
tsolovye <tsolovye@aa8a525f-4958-0410-8862-b993faadc08d>
2008-10-10 14:59:04 +0000

    Initial revision.
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@5699 aa8a525f-4958-0410-8862-b993faadc08d

---
+++ eijava/eilib/src/org/ei/data/paper/runtime/PaperChemDataDictionary.java
@@ -0,0 +1,51 @@
+package org.ei.data.paper.runtime;
+
+import org.ei.domain.DataDictionary;
+import java.util.Hashtable;
+
+
+public class PaperChemDataDictionary
+    implements DataDictionary
+{
+
+    private Hashtable treatmentCodes;
+
+    public Hashtable getTreatments()
+    {
+        return this.treatmentCodes;
+    }
+
+    public PaperChemDataDictionary()
+    {
+
+        treatmentCodes = new Hashtable();
+
+        treatmentCodes.put("A","Applications (APP)");
+        treatmentCodes.put("B","Biographical (BIO)");
+        treatmentCodes.put("E","Economic (ECO)");
+        treatmentCodes.put("X","Experimental (EXP)");
+        treatmentCodes.put("G","General review (GEN)");
+        treatmentCodes.put("H","Historical (HIS)");
+        treatmentCodes.put("L","Literature review (LIT)");
+        treatmentCodes.put("M","Management aspects (MAN)");
+        treatmentCodes.put("N","Numerical (NUM)");
+        treatmentCodes.put("T","Theoretical (THR)");
+
+    }
+
+	public Hashtable getAuthorityCodes()
+	{
+		return null;
+	}
+	public String getClassCodeTitle(String classCode)
+	{
+		return null;
+	}
+
+
+	public Hashtable getClassCodes()
+	{
+		return null;
+	}
+
+}
