95a52c28ba502c806832771eb2e8a5dc002e646a
hmo <hmo@aa8a525f-4958-0410-8862-b993faadc08d>
2008-10-13 17:33:40 +0000

    updated
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@5712 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/data/paper/loadtime/ExtractPaperChem.java
+++ eijava/eilib/src/org/ei/data/paper/loadtime/ExtractPaperChem.java
@@ -17,7 +17,7 @@ public class ExtractPaperChem
 {
 	public static void main(String[] args) throws Exception
 	{
-		String[] m_ids = new String[]{"pch_34f213f85aae815aM672219817173212","pch_B9CB8C08410F10C6E03408002081DCA4","pch_34f213f85aae815aM7e2b19817173212","pch_115f0a9f85ab60809M7ea819817173212","pch_B9CB8C083E7510C6E03408002081DCA4","pch_B9CB8C03873410C6E03408002081DCA4","pch_B9CB8C08184610C6E03408002081DCA4","pch_B9CB8C07B53010C6E03408002081DCA4","pch_B9CB8C0806B010C6E03408002081DCA4","pch_B9CB8C0806B110C6E03408002081DCA4","pch_34f213f85aae815aM672219817173212","pch_34f213f85aae815aM7e1a19817173212"};
+		String[] m_ids = new String[]{"pch_B9CB8C0218DB10C6E03408002081DCA4","pch_34f213f85aae815aM672219817173212","pch_B9CB8C08410F10C6E03408002081DCA4","pch_34f213f85aae815aM7e2b19817173212","pch_115f0a9f85ab60809M7ea819817173212","pch_B9CB8C083E7510C6E03408002081DCA4","pch_B9CB8C03873410C6E03408002081DCA4","pch_B9CB8C08184610C6E03408002081DCA4","pch_B9CB8C07B53010C6E03408002081DCA4","pch_B9CB8C0806B010C6E03408002081DCA4","pch_B9CB8C0806B110C6E03408002081DCA4","pch_34f213f85aae815aM672219817173212","pch_34f213f85aae815aM7e1a19817173212"};
 		Connection con = getDbCoonection("jdbc:oracle:thin:@neptune.elsevier.com:1521:EI", "AP_PRO1", "ei3it", "oracle.jdbc.driver.OracleDriver");
 		ExtractPaperChem epc = new ExtractPaperChem();
 		epc.extract(m_ids,con);
@@ -467,6 +467,10 @@ public class ExtractPaperChem
 		{
 			if(author.indexOf(";")>-1)
 			{
+				if(author.indexOf("&amp;")>-1)
+				{
+					author = author.replaceAll("&amp;","&");
+				}
 				authorArray = author.split(";",-1);
 			}
 			else
