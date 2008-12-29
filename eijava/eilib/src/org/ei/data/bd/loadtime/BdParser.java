1f74c2106c53879dc4445f131d3f7687c446f767
hmo <hmo@aa8a525f-4958-0410-8862-b993faadc08d>
2008-12-29 21:32:28 +0000

    update
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@5962 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/data/bd/loadtime/BdParser.java
+++ eijava/eilib/src/org/ei/data/bd/loadtime/BdParser.java
@@ -1753,7 +1753,7 @@ public class BdParser
 		 	switch (intMonth)
 		 	{
 		 		case 1:   monthString =  "January"; 	break;
-		 		case 2:   monthString =  "Febrary"; 	break;
+		 		case 2:   monthString =  "February"; 	break;
 		 		case 3:   monthString =  "March"; 		break;
 		 		case 4:   monthString =  "April"; 		break;
 		 		case 5:   monthString =  "May"; 		break;
