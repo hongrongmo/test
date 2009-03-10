aaf8efa47fb3bf9b2966750e6b465a9d99f2688b
Joel <Joel@aa8a525f-4958-0410-8862-b993faadc08d>
2009-03-10 15:57:34 +0000

    added the phrase boosting code back in
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@6207 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/query/base/PhraseBooster.java
+++ eijava/eilib/src/org/ei/query/base/PhraseBooster.java
@@ -1,14 +1,9 @@
 package org.ei.query.base;
 
-import org.ei.parser.base.AndPhrase;
-import org.ei.parser.base.AndQuery;
-import org.ei.parser.base.BaseNodeVisitor;
-import org.ei.parser.base.BooleanPhrase;
-import org.ei.parser.base.BooleanQuery;
-import org.ei.parser.base.Expression;
-import org.ei.parser.base.NotQuery;
-import org.ei.parser.base.OrPhrase;
-import org.ei.parser.base.OrQuery;
+import org.ei.parser.base.*;
+import org.ei.parser.*;
+import java.util.*;
+
 
 
 public class PhraseBooster
@@ -16,14 +11,10 @@ public class PhraseBooster
 {
 
     private FieldGetter fget = new FieldGetter();
-    private QueryRules rules;
     private TermGatherer gatherer = new TermGatherer();
     private String currentField;
+	private boolean done = false;
 
-    public PhraseBooster(QueryRules r)
-    {
-        rules = r;
-    }
 
     public BooleanQuery applyBoost(BooleanQuery bQuery)
     {
@@ -54,8 +45,7 @@ public class PhraseBooster
     public void visitWith(Expression exp)
     {
         String field = fget.getFieldValue(exp);
-        FieldRule rule = rules.getFieldRule(field);
-        if(rule.getAutoStem())
+        if(field.equalsIgnoreCase("all") || field.equalsIgnoreCase("ky"))
         {
             descend(exp);
         }
@@ -78,56 +68,47 @@ public class PhraseBooster
         descend(oPhrase);
     }
 
-/*
+
     public void visitWith(Phrase phrase)
     {
-        boolean shouldboost = true;
-        ParseNode parent = phrase.getParent();
-        ArrayList terms = gatherer.gatherTerms(phrase);
-        if(terms.size() > 1)
-        {
-            StringBuffer buf = new StringBuffer();
-            for(int i=0;i<terms.size();++i)
-            {
-                Term term = (Term)terms.get(i);
-                ParseNode pnode = term.getChildAt(0);
-                if(pnode.getType().equals(Regex.TYPE))
-                {
-                    //System.out.println("IS REGEX");
-                    if(((Regex)pnode).isStem())
-                    {
-                        //System.out.println("IS Stem");
-                        Literal l = (Literal)pnode.getChildAt(1);
-                        buf.append(l.getValue().trim()+" ");
-                    }
-                    else
-                    {
-                        shouldboost = false;
-                        break;
-                    }
-                }
-                else
-                {
-                    buf.append(term.getValue().trim()+" ");
-                }
-            }
+		if(!done)
+		{
+			boolean shouldboost = true;
+			ParseNode parent = phrase.getParent();
+			ArrayList terms = gatherer.gatherTerms(phrase);
+			if(terms.size() > 1)
+			{
+				StringBuffer buf = new StringBuffer();
+				for(int i=0;i<terms.size();++i)
+				{
+					Term term = (Term)terms.get(i);
+					ParseNode pnode = term.getChildAt(0);
+					if(pnode.getType().equals(Regex.TYPE))
+					{
+						shouldboost = false;
+						break;
+					}
+					else
+					{
+						buf.append(term.getValue().trim()+" ");
+					}
+				}
 
-            if(shouldboost)
-            {
-                ExactTerm eTerm = new ExactTerm(buf.toString().trim());
-                Term t = new Term(eTerm);
-                Phrase phrase2 = new Phrase(t);
-                BooleanOr b = new BooleanOr("OR");
-                OpenParen oParen = new OpenParen("(");
-                CloseParen cParen = new CloseParen(")");
-                OrPhrase oPhrase = new OrPhrase(new BooleanPhrase(phrase2), b, new BooleanPhrase(oParen,new BooleanPhrase(phrase),cParen));
-                BooleanPhrase bPhrase = new BooleanPhrase(oPhrase);
-                BooleanPhrase nPhrase = new BooleanPhrase(oParen, bPhrase, cParen);
-                parent.setChildAt(phrase.getChildIndex(), nPhrase);
-            }
-        }
-
-
+				if(shouldboost)
+				{
+					done = true;
+					ExactTerm eTerm = new ExactTerm(buf.toString().trim());
+					Term t = new Term(eTerm);
+					Phrase phrase2 = new Phrase(t);
+					BooleanOr b = new BooleanOr("OR");
+					OpenParen oParen = new OpenParen("(");
+					CloseParen cParen = new CloseParen(")");
+					OrPhrase oPhrase = new OrPhrase(new BooleanPhrase(phrase2), b, new BooleanPhrase(oParen,new BooleanPhrase(phrase),cParen));
+					BooleanPhrase bPhrase = new BooleanPhrase(oPhrase);
+					BooleanPhrase nPhrase = new BooleanPhrase(oParen, bPhrase, cParen);
+					parent.setChildAt(phrase.getChildIndex(), nPhrase);
+				}
+			}
+		}
     }
-*/
 }
