bef32fcb6e3c3d2a8729a3c1245236fb32763936
tsolovye <tsolovye@aa8a525f-4958-0410-8862-b993faadc08d>
2009-09-02 19:40:26 +0000

    concat data elems to map bdau
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@6495 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/data/inspec/runtime/InspecAuthors.java
+++ eijava/eilib/src/org/ei/data/inspec/runtime/InspecAuthors.java
@@ -21,22 +21,139 @@ public class InspecAuthors
 {
     private BdAuthors au ;
     private static ArrayList  auElements = new ArrayList();
+    
+    private static ArrayList  auElementsExpanded = new ArrayList();
+    private boolean isExpanded = false;
 
     static
     {      
         auElements.add("indexedName");
         auElements.add("affidStr");
-        //auElements.add("foreNames"); for 2010 addition
-       // auElements.add("initials");
-       // auElements.add("suffix");
-       //auElements.add("eAddress"); for 2010 addition
-       //auElements.add("id");for 2010 addition
+    }
+    
+    static
+    {      
+        auElementsExpanded.add("indexedName");
+        auElementsExpanded.add("affidStr");
+        auElementsExpanded.add("foreNames"); 
+        auElementsExpanded.add("initials");
+        auElementsExpanded.add("suffix");
+        auElementsExpanded.add("eAddress"); 
+        auElementsExpanded.add("id");
     }
 
-    public InspecAuthors(String bdAuthors)
+    public InspecAuthors(String dAus)
     { 
-    	au = new BdAuthors(bdAuthors,auElements);
+    	au = new BdAuthors(formatAuStr(dAus));  	
     }
+    
+    
+    public String formatAuStr(String dAus)
+    {
+    	String [] a = dAus.split(BdParser.IDDELIMITER);
+    
+    	if(a.length > 1)
+    	{
+    		isExpanded = true;
+    	}
+    	
+    	StringBuffer formatedData = new StringBuffer();
+    	
+    	String[] daus = dAus.split(BdParser.AUDELIMITER);   	
+    	for(int i = 0; i < daus.length; i++)
+    	{
+    		if(daus[i] != null && daus[i].length() > 0)
+    		{
+    			String []dau = daus[i].split(BdParser.IDDELIMITER);
+    			if(daus != null && daus.length > 0)
+    			{
+    				if(!isExpanded)
+    				{   //0
+    					formatedData.append(BdParser.IDDELIMITER);
+    					//1
+    					formatedData.append(BdParser.IDDELIMITER);
+    					//2
+    					if(dau.length >1 && dau[1] != null)
+    					{
+    						formatedData.append(dau[1].substring(dau[1].lastIndexOf(".")+1));
+    					}    				
+    					formatedData.append(BdParser.IDDELIMITER);
+    					if(dau.length >0 && dau[0] != null)
+    					{
+    						formatedData.append(dau[0]);
+    					}
+    					formatedData.append(BdParser.IDDELIMITER);
+    					formatedData.append(BdParser.IDDELIMITER);
+    					formatedData.append(BdParser.IDDELIMITER);
+    					formatedData.append(BdParser.IDDELIMITER);
+    					formatedData.append(BdParser.IDDELIMITER);
+    					formatedData.append(BdParser.IDDELIMITER);
+    					formatedData.append(BdParser.IDDELIMITER);   		
+    				}
+    				else
+    				{  
+    					//0
+    					formatedData.append(BdParser.IDDELIMITER);
+    					//1
+     					formatedData.append(BdParser.IDDELIMITER);
+    					//2
+     					if(dau.length >1 && dau[1] != null)
+    					{
+     						formatedData.append(dau[1].substring(dau[1].lastIndexOf(".")+1));
+    					}    				
+    					formatedData.append(BdParser.IDDELIMITER);
+    					//3
+    					if(dau.length >0 && dau[0] != null)
+    					{
+    						formatedData.append(dau[0]);
+    					}
+
+    					formatedData.append(BdParser.IDDELIMITER);
+    					//4
+    					if(dau.length >3 && dau[3] != null)
+    					{
+    						formatedData.append(dau[3]); //3
+    					}
+    					formatedData.append(BdParser.IDDELIMITER);
+    					//5
+
+    					formatedData.append(BdParser.IDDELIMITER);
+    					//6
+    					formatedData.append(BdParser.IDDELIMITER);
+    					//7
+    					formatedData.append(BdParser.IDDELIMITER);
+    					//8
+    					if(dau.length >4 && dau[4] != null)
+    					{
+    						formatedData.append(dau[4]);
+    					}
+    					formatedData.append(BdParser.IDDELIMITER);
+    					//9
+    					formatedData.append(BdParser.IDDELIMITER);
+    					//10
+    					formatedData.append(BdParser.IDDELIMITER);
+    					//11
+    					formatedData.append(BdParser.IDDELIMITER);
+    					//12
+    					formatedData.append(BdParser.IDDELIMITER);
+    					//13
+    					formatedData.append(BdParser.IDDELIMITER);
+    					if(dau.length >5 && dau[5] != null)
+    					{
+    						formatedData.append(dau[5]);
+    					}
+    				}    		
+    			}
+    		}   		
+    		formatedData.append(BdParser.AUDELIMITER);
+    	}
+    	
+    	return formatedData.toString();
+
+    }
+    
+    
+    
     
     public String[] getSearchValue()
     { 
