4a9f3a2a6b5a0d4c3a93d99abc62e2d9f97a975c
tsolovye <tsolovye@aa8a525f-4958-0410-8862-b993faadc08d>
2009-09-18 14:32:58 +0000

    Initial revision.
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@6514 aa8a525f-4958-0410-8862-b993faadc08d

---
+++ eijava/eilib/src/org/ei/data/bd/runtime/CRNumStrategy.java
@@ -0,0 +1,40 @@
+package org.ei.data.bd.runtime;
+
+
+public class CRNumStrategy 
+{	
+	private CRNumStrategy crn = null;
+	protected String crnumraw = null;
+	
+	public final int ELT_BIT_MASK = 1024;
+	public final int PCM_BIT_MASK = 54;
+	
+	public CRNumStrategy(){};
+	
+	public CRNumStrategy(String crnumRaw)
+	{
+		crnumraw = crnumRaw;
+	}
+
+	public String[] getCRN(int bitMask) 
+	{		
+		
+		if((ELT_BIT_MASK & bitMask) == ELT_BIT_MASK)
+		{
+			crn = new CRNumStrategyEncompass(crnumraw);
+			return crn.crnAlgorithm();
+		}
+		else if((PCM_BIT_MASK & bitMask) == PCM_BIT_MASK)
+		{
+			crn = new CRNumStrategyBData(crnumraw);
+			return crn.crnAlgorithm();
+		}
+		return null;
+	}
+	
+	public String[] crnAlgorithm()
+	{
+		return null;
+	}
+
+}
