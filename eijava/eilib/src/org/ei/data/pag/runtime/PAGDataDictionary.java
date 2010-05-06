b8807c440fa692fb8a710ae2e396748c9f517e17
tsolovye <tsolovye@aa8a525f-4958-0410-8862-b993faadc08d>
2010-05-06 15:50:18 +0000

    added implemented method getTreatmentTitle() defined in DataDictionary
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@6607 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/data/pag/runtime/PAGDataDictionary.java
+++ eijava/eilib/src/org/ei/data/pag/runtime/PAGDataDictionary.java
@@ -42,4 +42,8 @@ public class PAGDataDictionary  implements DataDictionary
     {
     	return null;
     }
+    public String getTreatmentTitle(String mTreatmentCode)
+    {
+    	return null;
+    }
 }
