b63f4b3d279656642350ddefb2b6cce1c1d87376
tsolovye <tsolovye@aa8a525f-4958-0410-8862-b993faadc08d>
2010-05-07 15:57:40 +0000

    Copyrights 2010
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@6616 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/controller/RSSBlocker.java
+++ eijava/eilib/src/org/ei/controller/RSSBlocker.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class RSSBlocker
 {
-	public static String BLOCKED_MESSAGE ="<rss version=\"2.0\"><channel><title>RSS feed blocked</title><link>http://www.engineeringvillage.com</link><description></description><language>en-us</language><item><title>This RSS feed has been blocked by Engineering Village</title><description>Your RSS reader has been configured to update this feed too frequently. Please change the settings on this feed to update once a day. To have this feed turned back on please contact eicustomersupport@elsevier.com.</description><link>http://www.engineeringvillage.com</link></item><copyright>Copyright 2009 Elsevier Inc.</copyright></channel></rss>";
+	public static String BLOCKED_MESSAGE ="<rss version=\"2.0\"><channel><title>RSS feed blocked</title><link>http://www.engineeringvillage.com</link><description></description><language>en-us</language><item><title>This RSS feed has been blocked by Engineering Village</title><description>Your RSS reader has been configured to update this feed too frequently. Please change the settings on this feed to update once a day. To have this feed turned back on please contact eicustomersupport@elsevier.com.</description><link>http://www.engineeringvillage.com</link></item><copyright>Copyright 2010 Elsevier Inc.</copyright></channel></rss>";
 	private static RSSBlocker instance;
 	private static long createTime;
 	private static long currentTime;
