b575ef7123a75f02a1505f673eadc16586ee04a9
hmo <hmo@aa8a525f-4958-0410-8862-b993faadc08d>
2010-08-16 17:14:44 +0000

    updated
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@6752 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/engvillage/engresources/web/js/citedby2.js
+++ eijava/engvillage/engresources/web/js/citedby2.js
@@ -65,32 +65,35 @@ function ajaxCitedByFunction()
 		//alert("count= "+data.result.length);
 		
 		for(i=0;i<data.result.length;i++)
-		{			
-		   var id = data.result[i].ID;
-		   //alert("DATA= "+id);
-		   var eid = data.result[i].EID;
-		   var scopusID = data.result[i].SID;
-		   var count = data.result[i].COUNT;
-		   var dashID=id+"dash";
-		   if(document.getElementById(id)!=null && document.getElementById(dashID)!=null)
-		   {			
-			document.getElementById(dashID).innerHTML = "&#160; - &#160";
-			var countString="Cited by in Scopus ("+count+")";
-			document.getElementById(id).innerHTML = countString;
-			if(count>1)
-			{
-				document.getElementById(id).title="Scopus found "+count+" citations for this article";
-			}
-			else
-			{
-				document.getElementById(id).title="Scopus found "+count+" citation for this article";
-			}
-			document.getElementById(id).href = "javascript:newwindow=window.open('http://www.scopus.com/scopus/inward/citedby.url?partnerID=qRss3amk&eid="+eid+"','newwindow','width=500,height=500,toolbar=no,location=no,scrollbars,resizable'); void('');";
-			
-		   }
-		   else
+		{
+		   if(data.result[i]!=null && data.result[i]!=undefined)
 		   {
-			alert("document.getElementById(id) is null");
+			   var id = data.result[i].ID;
+			   //alert("DATA= "+id);
+			   var eid = data.result[i].EID;
+			   var scopusID = data.result[i].SID;
+			   var count = data.result[i].COUNT;
+			   var dashID=id+"dash";
+			   if(document.getElementById(id)!=null && document.getElementById(dashID)!=null)
+			   {			
+				document.getElementById(dashID).innerHTML = "&#160; - &#160";
+				var countString="Cited by in Scopus ("+count+")";
+				document.getElementById(id).innerHTML = countString;
+				if(count>1)
+				{
+					document.getElementById(id).title="Scopus found "+count+" citations for this article";
+				}
+				else
+				{
+					document.getElementById(id).title="Scopus found "+count+" citation for this article";
+				}
+				document.getElementById(id).href = "javascript:newwindow=window.open('http://www.scopus.com/scopus/inward/citedby.url?partnerID=qRss3amk&eid="+eid+"','newwindow','width=500,height=500,toolbar=no,location=no,scrollbars,resizable'); void('');";
+
+			   }
+			   else
+			   {
+				alert("document.getElementById(id) is null");
+			   }
 		   }
 
 		}
