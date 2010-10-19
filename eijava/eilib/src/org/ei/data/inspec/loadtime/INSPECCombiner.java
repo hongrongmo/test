a0808b9b909b219dc44e690cffbad5f7fc4c5b1c
zyang <zyang@aa8a525f-4958-0410-8862-b993faadc08d>
2010-10-19 15:46:23 +0000

    2011 change
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@8031 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/src/org/ei/data/inspec/loadtime/INSPECCombiner.java
+++ eijava/eilib/src/org/ei/data/inspec/loadtime/INSPECCombiner.java
@@ -87,7 +87,7 @@ public class INSPECCombiner
         // extract the whole thing
         else if(loadNumber == 0)
         {
-        	for(int yearIndex = 2005; yearIndex <= 2010; yearIndex++)
+        	for(int yearIndex = 2005; yearIndex <= 2011; yearIndex++)
         	{
         		System.out.println("Processing year " + yearIndex + "...");
         		//create  a new writer so we can see the loadNumber/yearNumber in the filename
