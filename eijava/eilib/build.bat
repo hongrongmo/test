6c3b488313647ccb654a0d252a5ba4da593331a1
hmo <hmo@aa8a525f-4958-0410-8862-b993faadc08d>
2014-03-11 13:32:30 +0000

    updated
    git-svn-id: svn+ssh://neptune.elsevier.com/usr/local/subversion/repos/baja/trunk@23286 aa8a525f-4958-0410-8862-b993faadc08d

--- eijava/eilib/build.bat
+++ eijava/eilib/build.bat
@@ -1,7 +1,8 @@
 @echo off
 
 rem Execute ANT to perform the requird build target
-set JAVA_HOME=c:\j2sdk1.4.2_03
+rem set JAVA_HOME=c:\j2sdk1.4.2_03
+set JAVA_HOME=C:\jdk1.6.0_29
 %JAVA_HOME%\bin\java -version
 %JAVA_HOME%\bin\java -classpath ..\ant\ant.jar;..\ant\xerces.jar;%JAVA_HOME%\lib\tools.jar org.apache.tools.ant.Main -Dtomcat.home=%TOMCAT_HOME% %1 %2 %3 %4 %5 %6 %7 %8 %9
 
