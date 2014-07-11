bc20432942ac32a5c260eb394604295fdd14bd68
T.M Harover <harover@elsevier.com>
2014-07-11 16:19:33 -0400

    Remove error thrown to just a logged error.

--- ROOT-Maven/src/main/java/org/ei/service/properties/RuntimePropertiesServiceImpl.java
+++ ROOT-Maven/src/main/java/org/ei/service/properties/RuntimePropertiesServiceImpl.java
@@ -1,41 +1,41 @@
 package org.ei.service.properties;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.ei.service.properties.DAO.RuntimePropertiesDAO;
-import org.ei.service.properties.DAO.RuntimePropertiesDAOException;
 
+public class RuntimePropertiesServiceImpl implements RuntimePropertiesService {
+    public static final Logger log4j = Logger.getLogger(RuntimePropertiesServiceImpl.class.getName());
 
+    RuntimePropertiesDAO dao = null;
 
-public class RuntimePropertiesServiceImpl implements RuntimePropertiesService
-{
-	
-	RuntimePropertiesDAO dao = null; 
-	
-	public RuntimePropertiesServiceImpl(){
-		dao= new RuntimePropertiesDAO();
-	}
+    public RuntimePropertiesServiceImpl() {
+        dao = new RuntimePropertiesDAO();
+    }
 
-	public boolean getSSOCoreRedirectFlag() throws RuntimePropertiesServiceException{
-		
-		try {
-			String ssoCoreRedirectFlag = getDao().getSSOCoreRedirectFlag();
-			if(StringUtils.isNotBlank(ssoCoreRedirectFlag)){
-				return Boolean.valueOf(ssoCoreRedirectFlag);
-			}
-		} catch (RuntimePropertiesDAOException e) {
-			throw new RuntimePropertiesServiceException("error in fetching the SSO CORE REDIRECT FALG From Database", e);
-		}
-		
-		return false;
-	}
-	
-	
-	public RuntimePropertiesDAO getDao() {
-		return dao;
-	}
+    public boolean getSSOCoreRedirectFlag() throws RuntimePropertiesServiceException {
 
-	public void setDao(RuntimePropertiesDAO dao) {
-		this.dao = dao;
-	}
-	
+        try {
+            String ssoCoreRedirectFlag = getDao().getSSOCoreRedirectFlag();
+            if (StringUtils.isNotBlank(ssoCoreRedirectFlag)) {
+                return Boolean.valueOf(ssoCoreRedirectFlag);
+            }
+        } catch (Exception e) {
+            log4j.error("Error in fetching the SSO CORE REDIRECT FLAG From Database", e);
+            if (e.getCause() != null) {
+                log4j.error("Caused by: ", e.getCause());
+            }
+        }
+
+        return false;
+    }
+
+    public RuntimePropertiesDAO getDao() {
+        return dao;
+    }
+
+    public void setDao(RuntimePropertiesDAO dao) {
+        this.dao = dao;
+    }
+
 }
