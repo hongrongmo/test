30149348cd18c3041b0c0d8099ca9227d82a472f
Mohan Kamaraj <Mohan.Kamaraj@lexisnexis.com>
2014-07-31 16:17:03 -0400

    Fixing Update User Session issue when path choice exists

--- ROOT-Maven/src/main/java/org/ei/stripes/EVActionBeanContext.java
+++ ROOT-Maven/src/main/java/org/ei/stripes/EVActionBeanContext.java
@@ -111,7 +111,7 @@ public class EVActionBeanContext extends ActionBeanContext {
 	}
 	public UserSession updateUserSession(UserSession usersession) throws SessionException {
 		SessionManager sessionmanager = new SessionManager(getRequest(), getResponse());
-		return sessionmanager.updateUserSession(usersession, true);
+		return sessionmanager.updateUserSession(usersession, true, false);
 	}
 
     // The LogEntry object and getter
