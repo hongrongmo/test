a68bbbfe2e2f777870ff71a4960d7fb9a6b59d02
T.M Harover <harover@elsevier.com>
2014-08-19 13:16:22 -0400

    Fixing UTF-8 character encoding issue

--- ROOT-Maven/src/main/java/org/ei/query/base/EIQueryWriter.java
+++ ROOT-Maven/src/main/java/org/ei/query/base/EIQueryWriter.java
@@ -27,265 +27,185 @@ import org.ei.parser.base.StemmedTerm;
 import org.ei.parser.base.Term;
 import org.ei.parser.base.UnorderedNear;
 
-public class EIQueryWriter
-    extends QueryWriter
-{
+public class EIQueryWriter extends QueryWriter {
 
     private BufferStream buffer = new BufferStream();
 
-    public String getQuery(BooleanQuery bQuery)
-    {
+    public String getQuery(BooleanQuery bQuery) {
         bQuery.accept(this);
         return buffer.toString();
     }
 
-    public void visitWith(Expression exp)
-    {
+    public void visitWith(Expression exp) {
         descend(exp);
     }
 
-    public void visitWith(Field field)
-    {
-        try
-        {
-            buffer.write(" "+field.getNodeValue());
-        }
-        catch(Exception e)
-        {
+    public void visitWith(Field field) {
+        try {
+            buffer.write(" " + field.getNodeValue());
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-
-
-
-
-    public void visitWith(Phrase phrase)
-    {
-        try
-        {
+    public void visitWith(Phrase phrase) {
+        try {
             buffer.write(" ");
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         descend(phrase);
     }
 
-    public void visitWith(BooleanQuery bquery)
-    {
+    public void visitWith(BooleanQuery bquery) {
         descend(bquery);
     }
 
-
-    public void visitWith(OpenParen oParen)
-    {
-        try
-        {
+    public void visitWith(OpenParen oParen) {
+        try {
             buffer.write(" (");
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-
-    public void visitWith(Literal literal)
-    {
-        try
-        {
+    public void visitWith(Literal literal) {
+        try {
             buffer.write(literal.getValue().trim());
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void visitWith(KeywordWITHIN kWIHIN)
-    {
-        try
-        {
+    public void visitWith(KeywordWITHIN kWIHIN) {
+        try {
             buffer.write(" WN ");
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void visitWith(ProximityPhrase pp)
-    {
+    public void visitWith(ProximityPhrase pp) {
         descend(pp);
     }
 
-    public void visitWith(ProximityOperator po)
-    {
+    public void visitWith(ProximityOperator po) {
         descend(po);
     }
 
-    public void visitWith(OrderedNear o)
-    {
-        try
-        {
+    public void visitWith(OrderedNear o) {
+        try {
             buffer.write(" ONEAR");
-            if(o.getDistance() != 4)
-            {
+            if (o.getDistance() != 4) {
                 buffer.write("/");
                 buffer.write(Integer.toString(o.getDistance()));
             }
             buffer.write(" ");
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void visitWith(UnorderedNear o)
-    {
-        try
-        {
+    public void visitWith(UnorderedNear o) {
+        try {
             buffer.write(" NEAR");
-            if(o.getDistance() != 4)
-            {
+            if (o.getDistance() != 4) {
                 buffer.write("/");
                 buffer.write(Integer.toString(o.getDistance()));
             }
             buffer.write(" ");
 
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-
-    public void visitWith(StemmedTerm sTerm)
-    {
-        try
-        {
+    public void visitWith(StemmedTerm sTerm) {
+        try {
             buffer.write("$");
             descend(sTerm);
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void visitWith(ExactTerm eTerm)
-    {
-        try
-        {
-            buffer.write("{"+eTerm.getNodeValue()+"}");
-        }
-        catch(Exception e)
-        {
+    public void visitWith(ExactTerm eTerm) {
+        try {
+            buffer.write("{" + eTerm.getNodeValue() + "}");
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void visitWith(AndPhrase aPhrase)
-    {
+    public void visitWith(AndPhrase aPhrase) {
         descend(aPhrase);
     }
 
-    public void visitWith(OrPhrase oPhrase)
-    {
+    public void visitWith(OrPhrase oPhrase) {
         descend(oPhrase);
     }
 
-    public void visitWith(NotPhrase nPhrase)
-    {
+    public void visitWith(NotPhrase nPhrase) {
         descend(nPhrase);
     }
 
-    public void visitWith(AndQuery aQuery)
-    {
+    public void visitWith(AndQuery aQuery) {
         descend(aQuery);
     }
 
-    public void visitWith(OrQuery oQuery)
-    {
+    public void visitWith(OrQuery oQuery) {
         descend(oQuery);
     }
 
-    public void visitWith(NotQuery nQuery)
-    {
+    public void visitWith(NotQuery nQuery) {
         descend(nQuery);
     }
 
-    public void visitWith(Term term)
-    {
+    public void visitWith(Term term) {
         descend(term);
     }
 
-    public void visitWith(Regex reg)
-    {
-        try
-        {
+    public void visitWith(Regex reg) {
+        try {
             buffer.write(reg.getNodeValue());
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void visitWith(CloseParen cParen)
-    {
-        try
-        {
+    public void visitWith(CloseParen cParen) {
+        try {
             buffer.write(")");
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void visitWith(BooleanPhrase bPhrase)
-    {
+    public void visitWith(BooleanPhrase bPhrase) {
         descend(bPhrase);
     }
 
-    public void visitWith(BooleanAnd bAND)
-    {
-        try
-        {
+    public void visitWith(BooleanAnd bAND) {
+        try {
             buffer.write(" AND");
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void visitWith(BooleanOr bOR)
-    {
-        try
-        {
+    public void visitWith(BooleanOr bOR) {
+        try {
             buffer.write(" OR");
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void visitWith(BooleanNot bNOT)
-    {
-        try
-        {
+    public void visitWith(BooleanNot bNOT) {
+        try {
             buffer.write(" NOT");
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
