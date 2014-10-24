15dca715b02cb03c5fa8fd5c16635f7e802f1eae
Hanan Teleb <h.teleb@elsevier.com>
2014-10-24 14:56:18 -0400

    update cbnb Loadtime from SVN

--- eilib/src/main/java/org/ei/data/cbnb/loadtime/CBNBRecordFixer.java
+++ eilib/src/main/java/org/ei/data/cbnb/loadtime/CBNBRecordFixer.java
@@ -1,61 +1,84 @@
 package org.ei.data.cbnb.loadtime;
 
-import java.io.IOException;
-import java.util.ArrayList;
 
-import org.apache.oro.text.perl.Perl5Util;
+import java.util.*;
+import java.io.*;
+import org.apache.oro.text.perl.*;
+import org.apache.oro.text.regex.*;
+import org.ei.data.*;
 
-public class CBNBRecordFixer {
+
+public class CBNBRecordFixer
+{
 
     private Perl5Util perl = new Perl5Util();
 
     private String currentID;
-    private String pubyear = "";
+    private String pubyear= "";
 
-    public String fixRecord(String record) throws IOException {
+    public String fixRecord(String record)
+        throws IOException
+    {
         StringBuffer buf = new StringBuffer();
-        ArrayList<String> al = new ArrayList<String>();
+        ArrayList al = new ArrayList();
         perl.split(al, "/\t/", record.trim());
-        for (int i = 0; i < al.size(); ++i) {
-            if (i > 0) {
-                buf.append("	");
+        for(int i=0; i<al.size();++i)
+        {
+            if(i>0)
+            {
+                buf.append("    ");
             }
 
-            String fs = (String) al.get(i);
-            if (fs != null) {
-                buf.append(fixField(i, fs));
+            String fs = (String)al.get(i);
+            if(fs != null)
+            {
+                buf.append(fixField(i,fs));
             }
         }
 
         return buf.toString();
     }
 
-    private String fixField(int index, String data) throws IOException {
+
+    private String fixField(int index, String data)
+        throws IOException
+    {
 
         String fieldName = CBNBBaseTableRecord.baseTableFields[index];
 
-        if (data == null && !fieldName.equals("ABS")) {
+
+        if(data == null && !fieldName.equals("ABS"))
+        {
             return "";
         }
-        if (fieldName.equalsIgnoreCase("ABS")) {
-            if (data == null || data.trim().length() < 1) {
+        if(fieldName.equalsIgnoreCase("ABS"))
+        {
+            if(data == null || data.trim().length() < 1)
+            {
                 return "No abstract available";
             }
         }
 
-        if (fieldName.equalsIgnoreCase("PYR")) {
-            if (data == null || data.trim().length() < 1) {
+        if(fieldName.equalsIgnoreCase("PYR"))
+        {
+            if(data == null || data.trim().length() < 1)
+            {
                 return pubyear;
             }
         }
 
-        if (fieldName.equalsIgnoreCase("PBD") && data != null && data.trim().length() > 1) {
-            pubyear = data.substring(data.length() - 4);
+
+        if(fieldName.equalsIgnoreCase("PBD") && data != null && data.trim().length() > 1)
+        {
+            pubyear = data.substring(data.length()-4);
             return data;
         }
 
-        if (fieldName.equalsIgnoreCase("CDN")) {
-            if (data.equalsIgnoreCase("QQQQQQ")) {
+
+        if(fieldName.equalsIgnoreCase("CDN"))
+        {
+            if(data.equalsIgnoreCase("QQQQQQ"))
+            {
                 return "";
             }
 
