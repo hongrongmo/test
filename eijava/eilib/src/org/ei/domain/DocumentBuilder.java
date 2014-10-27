0ca5e558fe5cd729e2d5aaea8b0f1998af5c5f93
Hongrong Mo <hmo@elsevier.com>
2014-10-27 10:33:28 -0400

    update

--- eijava/eilib/src/org/ei/domain/DocumentBuilder.java
+++ eijava/eilib/src/org/ei/domain/DocumentBuilder.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.ei.domain;
 
 import java.util.List;
@@ -22,28 +21,3 @@ public interface DocumentBuilder
 	public DocumentBuilder newInstance(Database database);
 
 }
-=======
-package org.ei.domain;
-
-import java.util.List;
-
-
-/** This Interface is implemented by all the basic DocBuilders
-  *  like CPXChemDocBuilder,CNBNDocBuilder...
-  *  It provides a method buildPage which takes list of DocIds
-  *  and returns a List of EIDoc's
-  *
-  * @author Ravi Kumar Gullapalli
-  */
-
-public interface DocumentBuilder
-{
-
-	public List buildPage(List listOfDocID, 
-			      String dataFormat) 
-		throws DocumentBuilderException;
-	
-	public DocumentBuilder newInstance(Database database);
-
-}
->>>>>>> 15dca715b02cb03c5fa8fd5c16635f7e802f1eae
--- eijava/eilib/src/org/ei/domain/LocalHoldingLinker.java
+++ eijava/eilib/src/org/ei/domain/LocalHoldingLinker.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.ei.domain;
 
 public interface LocalHoldingLinker
@@ -30,35 +29,4 @@ public interface LocalHoldingLinker
 
     public static final String[] localHoldingFields = {AULAST,AUFIRST ,AUFULL,ISSN,ISBN1,ISBN2,ISSN9,CODEN, TITLE,STITLE,ATITLE,CTITLE, VOLUME2,VOLUME,ISSUE2,ISSUE,SPAGE,EPAGE,PAGES,YEAR,ANUMBER,DOI,ARTNUM,EISSN};
     public String getLocalHoldingLink(String URL);
-=======
-package org.ei.domain;
-
-public interface LocalHoldingLinker
-{
-    public static final String AULAST = "[AULAST]";
-    public static final String AUFIRST = "[AUFIRST]";
-    public static final String ISSN = "[ISSN]";
-    public static final String ISBN1 = "[ISBN1]";
-    public static final String ISBN2 = "[ISBN2]";
-    public static final String AUFULL = "[AUFULL]";
-    public static final String ISSN9 = "[ISSN9]";
-    public static final String EISSN = "[EISSN]";
-    public static final String CODEN = "[CODEN]";
-    public static final String TITLE = "[TITLE]";
-    public static final String STITLE = "[STITLE]";
-    public static final String ATITLE = "[ATITLE]";
-    public static final String CTITLE = "[CTITLE]";
-    public static final String VOLUME = "[VOLUME]";
-    public static final String ISSUE = "[ISSUE]";
-    public static final String SPAGE = "[SPAGE]";
-    public static final String EPAGE = "[EPAGE]";
-    public static final String PAGES = "[PAGES]";
-    public static final String ARTNUM = "[ARTNUM]";// The number of an individual item, in cases where there are no pages available.
-    public static final String YEAR = "[YEAR]";
-    public static final String ANUMBER = "[ANUMBER]";
-    public static final String DOI = "[DOI]";
-
-    public static final String[] localHoldingFields = {AULAST,AUFIRST ,AUFULL,ISSN,ISBN1,ISBN2,ISSN9,CODEN, TITLE,STITLE,ATITLE,CTITLE, VOLUME,ISSUE,SPAGE,EPAGE,PAGES,YEAR,ANUMBER,DOI,ARTNUM,EISSN};
-    public String getLocalHoldingLink(String URL);
->>>>>>> 15dca715b02cb03c5fa8fd5c16635f7e802f1eae
 }
