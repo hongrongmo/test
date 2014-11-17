2cc936ce28c9de0ede192783145a6d34d98a03e7
Hongrong Mo <hmo@elsevier.com>
2014-11-17 15:59:46 -0500

    add

--- eijava/eilib/build.bat
+++ eijava/eilib/build.bat
@@ -2,8 +2,9 @@
 
 rem Execute ANT to perform the requird build target
 rem set JAVA_HOME=c:\j2sdk1.4.2_03
-set JAVA_HOME=C:\jdk1.6.0_29
+rem set JAVA_HOME=C:\jdk1.6.0_29
+set JAVA_HOME=C:\jdk1.7.0_51
 %JAVA_HOME%\bin\java -version
-%JAVA_HOME%\bin\java -classpath ..\ant\ant.jar;..\ant\xerces.jar;%JAVA_HOME%\lib\tools.jar org.apache.tools.ant.Main -Dtomcat.home=%TOMCAT_HOME% %1 %2 %3 %4 %5 %6 %7 %8 %9
+%JAVA_HOME%\bin\java -classpath c:\baja\ant\ant.jar;c:\baja\ant\xerces.jar;%JAVA_HOME%\lib\tools.jar org.apache.tools.ant.Main -Dtomcat.home=%TOMCAT_HOME% %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 
