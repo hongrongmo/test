8df89b3bcea2f1b2a5cc2727923000ebd9500114
Hanan Teleb <h.teleb@elsevier.com>
2015-02-02 10:49:36 -0500

    Temp rollback INS class code change for GRF corr in NYC-staging

--- eilib/src/main/java/org/ei/data/insback/runtime/InsBackDatabase.java
+++ eilib/src/main/java/org/ei/data/insback/runtime/InsBackDatabase.java
@@ -17,9 +17,8 @@ import org.ei.exception.SearchException;
 import org.ei.fulldoc.LinkingStrategy;
 
 public class InsBackDatabase extends Database {
-	//HH 01/16/2015
-	private DataDictionary dataDictionary =  org.ei.data.inspec.runtime.InspecDataDictionary.getInstance();
-		
+	private DataDictionary dataDictionary = new org.ei.data.inspec.runtime.InspecDataDictionary();
+
 	public int getStartYear(boolean hasBackFile) {
 		return 1896;
 	}
