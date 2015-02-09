0a2ae8f18d434b365b2cf400662031bebbf62376
Hongrong Mo <hmo@elsevier.com>
2015-02-09 14:40:11 -0500

    added for cpx classification

--- eijava/eilib/src/org/ei/data/upt/loadtime/PatentXmlReader.bat
+++ eijava/eilib/src/org/ei/data/upt/loadtime/PatentXmlReader.bat
@@ -1,7 +1,7 @@
-set CLASSPATH= .;c:\baja\eijava\ext\xerces.jar;c:\jdk1.3.1_06\bin;C:\baja\eijava\dist\eilib.jar;c:\baja\eijava\ext\ojdbc14.jar;c:\baja\eijava\ext\xmlParserAPIs.jar;c:\baja\eijava\ext\oro.jar;c:\baja\eijava\ext\saxon.jar;c:\baja\eijava\ext\jdom.jar
+set CLASSPATH= .;c:\Users\moh\git\EngineeringVillage\eijava\ext\xerces.jar;c:\jdk1.3.1_06\bin;C:\Users\moh\git\EngineeringVillage\eijava\dist\eilib.jar;c:\Users\moh\git\EngineeringVillage\eijava\ext\ojdbc14.jar;c:\Users\moh\git\EngineeringVillage\eijava\ext\xmlParserAPIs.jar;c:\Users\moh\git\EngineeringVillage\eijava\ext\oro.jar;c:\Users\moh\git\EngineeringVillage\eijava\ext\saxon.jar;c:\Users\moh\git\EngineeringVillage\eijava\ext\jdom.jar
 
 set datafilename = 20080122_EP_Grants_BIBLIO_03
 set loadnumber = 200810
 echo %datafilename%
 echo %loadnumber%
-java org.ei.data.upt.loadtime.PatentXmlReader "C:\Documents and Settings\moh\My Documents\patent_document\zipfile\EP\grants" 200813_EP_Grants 200813
+java org.ei.data.upt.loadtime.PatentXmlReader in out 201500
