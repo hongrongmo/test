14d78decf88f719bcea248f139d2d795759faec2
Hongrong Mo <hmo@elsevier.com>
2015-02-26 15:33:58 -0500

    update

--- eilib/src/main/java/org/ei/data/inspec/runtime/InspecDataDictionary.java
+++ eilib/src/main/java/org/ei/data/inspec/runtime/InspecDataDictionary.java
@@ -1,5 +1,6 @@
 package org.ei.data.inspec.runtime;
 
+import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -16,9 +17,9 @@ public class InspecDataDictionary
     private static Hashtable<String, String> classCodes;
     private Hashtable<String, String> treatmentCodes;
 
-    
+
     //HH 01/16/2015
-    private static InspecDataDictionary instance = null; 
+    private static InspecDataDictionary instance = null;
     public static InspecDataDictionary getInstance()
     {
     	if(instance == null)
@@ -30,7 +31,7 @@ public class InspecDataDictionary
     			{
     				classCodes = instance.loadClassCodes();
     			}
-    			
+
     			catch(Exception e)
     			{
     				e.printStackTrace();
@@ -39,8 +40,8 @@ public class InspecDataDictionary
     	}
     	return instance;
     }
-    
-    
+
+
     public String getClassCodeTitle(String classCode)
     {
         //HH 01/14/2015
@@ -50,14 +51,14 @@ public class InspecDataDictionary
             if (classCode != null)
             {
                 classCodeTitle = (String)classCodes.get(classCode.toUpperCase());
-               
+
             }
         }
         catch(Exception e)
         {
             e.printStackTrace();
         }
-            
+
         return classCodeTitle;
     }
 
@@ -71,9 +72,9 @@ public class InspecDataDictionary
         return this.treatmentCodes;
     }
 
-    public InspecDataDictionary() 
+    public InspecDataDictionary()
     {
-       
+
        treatmentCodes = new Hashtable<String, String>();
 
        treatmentCodes.put("A","Applications (APP)");
@@ -100,35 +101,49 @@ public class InspecDataDictionary
     {
         return null;
     }
-    
-    
+
+
     /****
      * Author: HT
      * Date: 01/14/2015
      * Desc: retrieve Ins Class codes & title dynamically from DB table instead of statically specified in Prog
      */
-    
+
     public Hashtable<String, String> loadClassCodes ()
                         throws ThesaurusException
     {
         Hashtable<String,String> classCodesList = new Hashtable<String,String>();
-        
+
         ConnectionBroker broker = null;
         Connection con = null;
         Statement stmt = null;
         ResultSet rs = null;
-        
-        
+
+
         StringBuffer buf = new StringBuffer("select class_code,class_title from INSPEC_CLASSIFICATION");
-        
+
         try
         {
-            broker = ConnectionBroker.getInstance();
-            con = broker.getConnection(DatabaseConfig.SEARCH_POOL);
+
+			try
+			{
+				broker = ConnectionBroker.getInstance();
+            	con = broker.getConnection(DatabaseConfig.SEARCH_POOL);
+			}
+			catch(Exception	 e)
+			{
+				//added for dataloading, need to think about it what to do later
+				//e.printStackTrace();
+				System.out.println("running from dataloading");
+				if(con == null)
+				{
+					con = getConnection();
+				}
+			}
             stmt = con.createStatement();
-            
+
             rs = stmt.executeQuery(buf.toString());
-            
+
             while (rs.next())
             {
                 if((rs.getString("CLASS_CODE") !=null) && (rs.getString("CLASS_TITLE") !=null))
@@ -137,14 +152,14 @@ public class InspecDataDictionary
                 }
 
             }
-            
+
         }
         catch(Exception ex)
         {
-            
+
             throw new ThesaurusException(ex);
         }
-        
+
         finally
         {
             if(rs !=null)
@@ -157,9 +172,9 @@ public class InspecDataDictionary
                 {
                     e1.printStackTrace();
                 }
-                
+
             }
-        
+
             if(stmt !=null)
             {
                 try
@@ -171,15 +186,38 @@ public class InspecDataDictionary
                     e2.printStackTrace();
                 }
             }
-        
+
             if (con != null) {
                 try {
-                    broker.replaceConnection(con, DatabaseConfig.SEARCH_POOL);
+					if(broker!=null)
+                    	broker.replaceConnection(con, DatabaseConfig.SEARCH_POOL);
+					else
+						con.close();
                 } catch (Exception cpe) {
                     cpe.printStackTrace();
                 }
             }
         }
         return classCodesList;
-    } 
+    }
+
+	public Connection getConnection() throws Exception
+	{
+
+			String connectionURL="jdbc:oracle:thin:@eid.cmdvszxph9cf.us-east-1.rds.amazonaws.com:1521:eid";//used for cloud dataloading
+			//String connectionURL="jdbc:oracle:thin:@127.0.0.1:5521:eid";//use for local machine
+			String driver="oracle.jdbc.driver.OracleDriver";
+			String username="ap_ev_search";
+			String password="ei3it";
+			System.out.println("connectionURL= "+connectionURL);
+			System.out.println("driver= "+driver);
+			System.out.println("username= "+username);
+			System.out.println("password= "+password);
+
+			Class.forName(driver);
+			Connection con = DriverManager.getConnection(connectionURL,
+										  username,
+										  password);
+			return con;
+	}
 }
