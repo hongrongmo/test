c208a91ae2e340b8d6e505e349ed41b39aac3cf2
Hongrong Mo <hmo@elsevier.com>
2016-08-31 13:32:26 -0400

    add numerical index

--- dataloading-core/src/main/java/org/ei/dataloading/inspec/loadtime/InspecBaseTableWriter.java
+++ dataloading-core/src/main/java/org/ei/dataloading/inspec/loadtime/InspecBaseTableWriter.java
@@ -13,6 +13,7 @@ public class InspecBaseTableWriter
 	private int curRecNum = 0;
 	private String filename;
 	private PrintWriter out;
+	private PrintWriter numericalIndexOut;
 	private String filepath;
 	private int loadnumber;
 	private int filenumber = 0;
@@ -78,9 +79,17 @@ public class InspecBaseTableWriter
 			StringBuffer value = (StringBuffer) record.get(bf);
 			//System.out.println("Value= "+value);
 			String valueS = null;
+			String mid = null;
+			String accessnumber = null;
+			String loadnumber = null;
 			if(value != null)
 			{
 				valueS = value.toString();
+				
+				mid = record.get("M_ID").toString();		
+				accessnumber = record.get("ANUM").toString();								
+				loadnumber = record.get("LOAD_NUMBER").toString();
+								
 				if(valueS.length() == 0 && bf.equalsIgnoreCase("AB"))
 				{
 					valueS = NOABSTRACT;
@@ -102,6 +111,13 @@ public class InspecBaseTableWriter
 				    // fix last author in string to make sure it isn't a partial author name
 				    valueS = fixAuthors(valueS);
                 }
+				
+				if(bf.equalsIgnoreCase("NDI"))
+				{
+				    // output Numerical Index
+				    outPutNumericalIndex(mid,accessnumber,loadnumber,valueS);
+                }
+				
 			}
 			else
 			{
@@ -124,7 +140,99 @@ public class InspecBaseTableWriter
 		}
 		out.println("");
 		out.flush();
+		if(numericalIndexOut!=null)
+		{
+			numericalIndexOut.flush();
+		}
 		++curRecNum;
+	}
+	
+	public void outPutNumericalIndex(String mid, String accessnumber, String loadnumber, String valueS) throws Exception
+	{
+		if(numericalIndexOut==null)
+		{
+			numericalIndexOut = new PrintWriter(new FileWriter("Numerical_"+this.filename+".out")); 
+			System.out.println("create data file");
+		}
+		String[] numericalIndexRecords = valueS.split(Constants.AUDELIMITER);
+		for(int i=0;i<numericalIndexRecords.length;i++)
+		{
+			String numericalIndexRecord = numericalIndexRecords[i];
+			if(numericalIndexRecord!=null && numericalIndexRecord.length()>0)
+			{
+				String[] subItems = numericalIndexRecord.split(Constants.IDDELIMITER);
+				if(subItems.length>2)
+				{
+					String quan = subItems[0];
+					String value1 = subItems[1];
+					String value2 = null;
+					String unit = null;
+					if(subItems.length>3)
+					{
+						value2 = subItems[2];
+						unit = subItems[3];
+					}
+					else
+					{
+						value2 = subItems[1];
+						unit = subItems[2];
+					}
+					
+					if(value2==null || value2.length()==0)
+					{
+						value2 = value1;
+					}
+					else if(value1==null || value1.length()==0)
+					{
+						value1 = value2;	
+					}
+					
+					if(isNumeric(value1) && isNumeric(value2))
+					{
+						if(Double.parseDouble(value1)>Double.parseDouble(value2))
+						{
+							String temp = value2;
+							value2 = value1;
+							value1 = temp;
+						}
+					}
+					else
+					{
+						System.out.println("either " +value1+ " or "+value2 +" is not numerical value");
+					}
+					
+					numericalIndexOut.write(mid+"\t"+accessnumber+"\t"+quan+"\t"+value1+"\t"+value2+"\t"+unit+"\t"+loadnumber+"\n");
+					
+				}
+				else
+				{
+					System.out.println("record "+mid+" in wrong numerical format SIZE="+subItems.length+" String="+numericalIndexRecord);
+				}
+				
+			}
+			else
+			{
+				System.out.println("PROBLEM in 1");
+			}
+			
+		}
+		numericalIndexOut.flush();
+		
+	}
+	
+	public boolean isNumeric(String str)  
+	{  
+	  try  
+	  {  
+	    double d = Double.parseDouble(str);  
+	  }  
+	  catch(NumberFormatException nfe)  
+	  {  
+		System.out.println("problem on "+str);
+		nfe.printStackTrace();  
+	    return false;  
+	  }  
+	  return true;  
 	}
 
 	private String fixAuthors(String authors)
@@ -148,6 +256,11 @@ public class InspecBaseTableWriter
 		{
 			out.close();
 			open = false;
+		}
+		
+		if(numericalIndexOut!=null)
+		{
+			numericalIndexOut.close();
 		}
 	}
 }
