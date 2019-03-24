a05712c7c52db52a152f2a499d5ab30253452212
Hanan Teleb <h.teleb@elsevier.com>
2019-03-24 15:32:50 -0400

    INITIAL VERSION

--- awsLambda/lambda/src/test/java/SumIntegerDigits.java
+++ awsLambda/lambda/src/test/java/SumIntegerDigits.java
@@ -20,49 +20,80 @@ public class SumIntegerDigits
 
 	public static void main(String[] args)
 	{
-		int x,y=0,z=0;
+		int x,y=0,z=0,i=0, curr;
+		Scanner scan = null;
 		Label label1 = null, label2 = null;
 		WritableWorkbook workBook = null;
-		
+
 		try
 		{
-			
+
 			File out = new File("AK.xls");
+
 			workBook = Workbook.createWorkbook(out);
-			WritableSheet sheet = workBook.createSheet("Sample1", 0);
+			WritableSheet sheet = workBook.createSheet("sample1", 0);
 			Map <Integer,Integer> freq = new HashMap<Integer, Integer>();
-			
+
 			SumIntegerDigits obj = new SumIntegerDigits();
-			
-			Scanner scan = new Scanner(System.in);
-			x = scan.nextInt();
-			
-			
-			if(obj.isValid(x))
+
+			scan = new Scanner(System.in);
+
+			sheet.addCell(new Label(0, 0, "X"));
+			sheet.addCell(new Label(1,0, "Y"));
+
+			if(out.exists() && !(out.isDirectory()))
+
 			{
-				
-				sheet.addCell(new Label(0, 0, "X"));
-				sheet.addCell(new Label(1,0, "Y"));
-				
-				while (x !=0 && !(freq.containsValue(2)))
+				while ((x = scan.nextInt()) !=0)
 				{
-					if(x >0)
-						x = x - Integer.valueOf(new StringBuffer(Integer.toString(Math.abs(x))).reverse().toString());
-					else if(x <0)
-						x = x + Integer.valueOf(new StringBuffer(Integer.toString(Math.abs(x))).reverse().toString());
-						
-					if(freq.containsKey(x))
-						freq.put(x,freq.get(x) +1);
+					if(obj.isValid(x))
+					{
+						freq.clear();
+
+						//sheet.addCell(new Label());
+						while (x !=0 && !(freq.containsValue(2)))
+						{
+							curr = x;
+							i++;
+
+							if(x >0)
+								x = x - Integer.valueOf(new StringBuffer(Integer.toString(Math.abs(x))).reverse().toString());
+							else if(x <0)
+								x = x + Integer.valueOf(new StringBuffer(Integer.toString(Math.abs(x))).reverse().toString());
+
+							if(freq.containsKey(x))
+								freq.put(x,freq.get(x) +1);
+							else
+								freq.put(x,1);
+
+							System.out.println(x);
+
+
+							sheet.addCell(new Label(0,i,Integer.toString(curr))); 
+							sheet.addCell(new Label(1,i,Integer.toString(x)));
+							
+							//System.out.println("Row#: " + i);   // only for debugging
+
+						}
+
+						System.out.println("frequency: " + freq);
+
+						i++;
+						// add blank line to separate between multiple inputs
+						sheet.addCell(new Label(0,i,"")); 
+						sheet.addCell(new Label(1,i,""));
+					}
 					else
-						freq.put(x,1);
-					
-					System.out.println(x);
+						System.out.println("Invalid number, try again with non zero, and diff digits number");
+
+
+
 				}
 				
+				workBook.write();
+
 			}
-			
-			
-			System.out.println("frequency: " + freq);
+
 		}
 		catch(InputMismatchException ex)
 		{
@@ -76,9 +107,40 @@ public class SumIntegerDigits
 		{
 			e.printStackTrace();
 		}
-		
+
+		finally
+		{
+			if(workBook !=null)
+			{
+				try
+				{
+					workBook.close();
+				}
+				catch(Exception e)
+				{
+					System.out.println("Failed to closed teh workbok!");
+					e.printStackTrace();
+				}
+			}
+
+			if(scan !=null)
+			{
+				try
+				{
+					scan.close();
+				}
+				catch(Exception e)
+				{
+					System.out.println("Failed to close the scanner!");
+					e.printStackTrace();
+				}
+
+			}
+
+		}
+
 	}
-	
+
 	public boolean isValid(int x)
 	{
 		/*
@@ -90,7 +152,7 @@ public class SumIntegerDigits
 		 * 
 		 * }
 		 */
-		
+
 		if(x!= 0 && x >0 && !(Integer.toString(x).matches("^(\\d)\\1*$")))
 			return true;
 		return false;
