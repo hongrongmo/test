7f310c1be58217c18b602878d97a414cdf27ba6e
Hanan Teleb <h.teleb@elsevier.com>
2019-05-06 16:01:32 -0400

    update

--- dataloading-core/src/main/java/org/ei/dataloading/upt/loadtime/vtw/DownloadVtwFile.java
+++ dataloading-core/src/main/java/org/ei/dataloading/upt/loadtime/vtw/DownloadVtwFile.java
@@ -15,10 +15,6 @@ import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.apache.commons.httpclient.params.HttpMethodParams;
 import org.apache.commons.io.ByteOrderMark;
 import org.apache.commons.io.IOUtils;
@@ -39,10 +35,6 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 import org.apache.wink.common.model.wadl.HTTPMethods;
-import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-
 import com.amazonaws.ClientConfiguration;
 
 //import sun.awt.image.ByteArrayImageSource;
