2085aa5f032182ab71099fcd661b1fcc9409e2cf
Hanan Teleb <h.teleb@elsevier.com>
2019-10-01 17:00:25 -0400

    Hanan local testing files
    Former-commit-id: 5406d8e96f20140378ef668578fdbc545e04c2da

---
+++ dataloading-core/src/main/java/org/ei/dataloading/kafka/Producer.java
@@ -0,0 +1,37 @@
+package org.ei.dataloading.kafka;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class Producer 
+{
+
+	private static String bootStrapServer = "localhost:9092";
+	
+	private  static KafkaProducer<String, String> prorducer = null;
+	
+	private Producer()
+	{
+		
+	}
+	
+	// static method to create instance of singleton KafkaProducer class 
+	
+	@SuppressWarnings("unchecked")
+	public static KafkaProducer<String,String> getInstance() {
+		if(prorducer == null)
+		{
+			Properties properties = new Properties();
+			properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServer);
+			properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+			properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+			prorducer = new KafkaProducer<String,String>(properties);
+		}
+		return prorducer;
+	}
+	
+	
+}
