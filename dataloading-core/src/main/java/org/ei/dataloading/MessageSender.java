ce72d5688a38f2fcc4dfcc1bb22ee7459a2f6f05
Hanan <h.teleb@elsevier.com>
2020-06-10 19:30:33 -0400

    adding missing file
    Former-commit-id: a774e89a9ba0532e87341b7f05c5a1be0be7ba35

---
+++ dataloading-core/src/main/java/org/ei/dataloading/MessageSender.java
@@ -0,0 +1,53 @@
+package org.ei.dataloading;
+
+import org.ei.util.kafka.KafkaService;
+
+public class MessageSender implements Runnable {
+  EVCombinedRec[] recArray;
+  EVCombinedRec rec;
+  
+  public MessageSender(EVCombinedRec[] recArray, KafkaService kafka, CombinedWriter writer) {
+    this.recArray = null;
+    this.rec = null;
+    this.kafka = null;
+    this.writer = null;
+
+    
+    this.recArray = recArray;
+    this.kafka = kafka;
+    this.writer = writer;
+  } KafkaService kafka; CombinedWriter writer; public MessageSender(EVCombinedRec rec, KafkaService kafka, CombinedWriter writer) {
+    this.recArray = null;
+    this.rec = null;
+    this.kafka = null;
+    this.writer = null;
+    this.rec = rec;
+    this.kafka = kafka;
+    this.writer = writer;
+  }
+
+  
+  public void run() {
+    try {
+      if (this.rec != null)
+      {
+        this.writer.writeRec(this.rec, this.kafka);
+        this.kafka.flush();
+      }
+      else if (this.recArray != null)
+      {
+        this.writer.writeRec(this.recArray, this.kafka);
+        this.kafka.flush();
+      }
+      else
+      {
+        System.out.println("NOTHING to SEND");
+      }
+    
+    }
+    catch (Exception e) {
+      
+      e.printStackTrace();
+    } 
+  }
+}
