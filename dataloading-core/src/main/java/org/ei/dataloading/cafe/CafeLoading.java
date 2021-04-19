c75d2d95ea4865e21f38c8749b89aaebd14dcb17
Hongrong Mo <h.mo@elsevier.com>
2021-04-19 09:26:23 -0400

    update

--- dataloading-core/src/main/java/org/ei/dataloading/cafe/CafeLoading.java
+++ dataloading-core/src/main/java/org/ei/dataloading/cafe/CafeLoading.java
@@ -1,6 +1,14 @@
 package org.ei.dataloading.cafe;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,13 +16,21 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.ei.dataloading.awss3.AmazonS3Service;
 import org.ei.dataloading.bd.loadtime.BaseTableDriver;
 import org.ei.dataloading.bd.loadtime.BaseTableWriter;
+import org.ei.dataloading.bd.loadtime.XmlCombiner;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
 
 public class CafeLoading
 {
 	private static BaseTableWriter baseWriter;
-    private int loadNumber;
+    private String loadNumber;
     private String databaseName;
     private String action;
     private static String startRootElement ="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><bibdataset xmlns:xocs=\"http://www.elsevier.com/xml/xocs/dtd\" xmlns:ce=\"http://www.elsevier.com/xml/common/dtd\" xmlns:xoe=\"http://www.elsevier.com/xml/xoe/dtd\" xmlns:aii=\"http://www.elsevier.com/xml/ani/internal\" xmlns:mml=\"http://www.w3.org/1998/Math/MathML\" xmlns:ait=\"http://www.elsevier.com/xml/ait/dtd\">";
@@ -85,6 +101,11 @@ public class CafeLoading
             con = c.getConnection(url,driver,username,password);
             c.setBlockedIssnList(con);
             List cafekeyList = c.getCafeKeys(archivedDate,con);
+            for(int i=0;i<cafekeyList.size();i++)
+            {
+            	String cafeKey = (String)cafekeyList.get(i);
+            	String cafeDoc = getCafeDocFromS3(cafeKey);
+            }
         }
         catch(Exception e)
         {
@@ -107,27 +128,185 @@ public class CafeLoading
         }
     }
     
+    private String getCafeDocFromS3(String key)
+    {
+    	StringBuffer docBuffer = new StringBuffer();
+    	AmazonS3 s3Client;
+		S3Object object;
+		try 
+		{
+			s3Client = AmazonS3Service.getInstance().getAmazonS3Service();			
+			object = s3Client.getObject( new GetObjectRequest("sccontent-ani-xml-prod", key));
+			InputStream objectData = object.getObjectContent();
+			System.out.println("Content-Type: "+object.getObjectMetadata().getContentType());
+	         //displayTextInputStream(object.getObjectContent());
+			 saveContentToFile(object.getObjectContent(),key);
+	         
+			// Process the objectData stream.
+			objectData.close();
+		 } 
+		catch (AmazonServiceException ase) 
+		{
+	            System.out.println("Caught an AmazonServiceException, which" +
+	            		" means your request made it " +
+	                    "to Amazon S3, but was rejected with an error response" +
+	                    " for some reason.");
+	            System.out.println("Error Message:    " + ase.getMessage());
+	            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+	            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+	            System.out.println("Error Type:       " + ase.getErrorType());
+	            System.out.println("Request ID:       " + ase.getRequestId());
+	        } catch (AmazonClientException ace) {
+	            System.out.println("Caught an AmazonClientException, which means"+
+	            		" the client encountered " +
+	                    "an internal error while trying to " +
+	                    "communicate with S3, " +
+	                    "such as not being able to access the network.");
+	            System.out.println("Error Message: " + ace.getMessage());
+	        }catch(Exception ioe)
+	        {
+	        	System.out.println("Other Error Message: " + ioe.getMessage());
+	        }
+    	return docBuffer.toString();
+    }
+    
+    public static void saveContentToFile (InputStream objectData, String key) throws IOException
+	{
+		BufferedReader breader = null;
+		PrintWriter out = null;
+		try
+		{
+			breader = new BufferedReader(new InputStreamReader(objectData));
+			breader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(breader.readLine().replaceAll("><", ">\n<").getBytes())));
+
+			//File file = new File(s3Dir.getName()+"/"+key+".xml");
+			File file = new File("./cafeData/"+key+".xml");
+
+			if (!file.exists()) 
+			{
+				System.out.println("Downloaded: "+file.getName());
+
+			}
+			else
+			{
+				System.out.println("file:" +  file.getName() + "already exist");
+			}
+
+			String line = null;
+			out = new PrintWriter(new BufferedWriter(new FileWriter(file.getAbsolutePath(),true)));
+			while ((line = breader.readLine()) !=null)
+			{
+				out.println(line);
+
+			}
+
+		}
+		catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		finally
+		{
+			try
+			{
+				if(breader !=null)
+				{
+					breader.close();
+				}
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+
+
+			try
+			{
+				if(out !=null)
+				{
+					out.flush();
+					out.close();
+				}
+			}
+			catch(Exception ex)
+			{
+				ex.printStackTrace();
+			}
+		}
+
+	}
+    
     private List getCafeKeys(String archivedDate,Connection con)
     {
     	List cafeDataKeyList = new ArrayList();
+        Statement stmt = null;
+        ResultSet rs = null;
+        try
+        {
+            stmt = con.createStatement();                              
+            String sqlQuery = "select * from cafe_inventory_temp where to_char(archive_date,'yyyymmdd')='"+archivedDate+"'";           
+            System.out.println("SQLQUERY= "+sqlQuery);
+            rs = stmt.executeQuery(sqlQuery);   
+            int i=0;
+            while(rs.next()) 
+            {
+            	cafeDataKeyList.add(rs.getString("key"));          	
+            } 
+        }
+        catch(Exception e)
+        {
+        	e.printStackTrace();
+        }
+        finally
+        {
+
+            if (rs != null)
+            {
+                try
+                {
+                    rs.close();
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+
+            if (stmt != null)
+            {
+                try
+                {
+                    stmt.close();
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
     	return cafeDataKeyList;
     }
     
-    public CafeLoading(int loadN,String databaseName)
+    public CafeLoading(String loadN,String databaseName)
     {
         this.loadNumber = loadN;
         this.databaseName = databaseName;
         this.action = "normal";
     }
 
-    public CafeLoading(int loadN,String databaseName,String action)
+    public CafeLoading(String loadN,String databaseName,String action)
     {
         this.loadNumber = loadN;
         this.databaseName = databaseName;
         this.action = action;
     }
     
-    public CafeLoading(int loadN,String databaseName, String action, String fileLocation)
+    public CafeLoading(String loadN,String databaseName, String action, String fileLocation)
     {
         this.loadNumber = loadN;
         this.databaseName = databaseName;
